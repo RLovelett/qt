dbf976c606202ffb806c1773eebbb81deed460d1
Kjell Rune Skaaraas <kjella79@yahoo.no>
2009-06-13 17:05:52 +0200

    Remove dependence on qsslsocket, qopenssl is now standalone

--- src/network/ssl/qopenssl_symbols_p.h
+++ src/network/ssl/qopenssl_symbols_p.h
@@ -54,7 +54,20 @@
 // We mean it.
 //
 
-#include "qsslsocket_openssl_p.h"
+#include <openssl/asn1.h>
+#include <openssl/bio.h>
+#include <openssl/bn.h>
+#include <openssl/err.h>
+#include <openssl/evp.h>
+#include <openssl/pem.h>
+#include <openssl/pkcs12.h>
+#include <openssl/pkcs7.h>
+#include <openssl/rand.h>
+#include <openssl/ssl.h>
+#include <openssl/stack.h>
+#include <openssl/x509.h>
+#include <openssl/x509v3.h>
+#include <openssl/x509_vfy.h>
 
 QT_BEGIN_NAMESPACE
 
