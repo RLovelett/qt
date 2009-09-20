b50916721f76d21b2a24ca7baa44f47cf9f37f27
Antonio Aloisio <antonio.aloisio@gmail.com>
2009-09-20 22:44:54 +0300

    Fix style for widgets using HildonEntry style. Note: Melody theme lacks of the combo box 'evil hacks', then Disabled comboBoxes looks ugly if Melody is enabled

--- src/gui/styles/qhildonstyle.cpp
+++ src/gui/styles/qhildonstyle.cpp
@@ -38,14 +38,17 @@
 
 #include <QtGui/QStyleOption>
 #include <QDebug>
-
+#include <qpixmapcache.h>
+#undef signals // Collides with GTK stymbols
 #include "qgtkpainter_p.h"
 
+#include <private/qcleanlooksstyle_p.h>
 #if !defined(QT_NO_STYLE_HILDON) && defined(Q_WS_HILDON)
 
 QT_BEGIN_NAMESPACE
 
-//Belongs to QGTKStyle
+static bool UsePixmapCache = true;
+
 static QColor mergedColors(const QColor &colorA, const QColor &colorB, int factor = 50)
 {
     const int maxFactor = 100;
@@ -53,6 +56,17 @@ static QColor mergedColors(const QColor &colorA, const QColor &colorB, int facto
     tmp.setRed((tmp.red() * factor) / maxFactor + (colorB.red() * (maxFactor - factor)) / maxFactor);
     tmp.setGreen((tmp.green() * factor) / maxFactor + (colorB.green() * (maxFactor - factor)) / maxFactor);
     tmp.setBlue((tmp.blue() * factor) / maxFactor + (colorB.blue() * (maxFactor - factor)) / maxFactor);
+    return tmp;
+}
+
+// Note this is different from uniqueName as used in QGtkPainter
+static QString uniqueName(const QString &key, const QStyleOption *option, const QSize &size)
+{
+    QString tmp;
+    const QStyleOptionComplex *complexOption = qstyleoption_cast<const QStyleOptionComplex *>(option);
+    tmp.sprintf("%s-%d-%d-%d-%lld-%dx%d", key.toLatin1().constData(), uint(option->state),
+                option->direction, complexOption ? uint(complexOption->activeSubControls) : uint(0),
+                option->palette.cacheKey(), size.width(), size.height());
     return tmp;
 }
 
@@ -239,16 +253,21 @@ void QHildonStyle::drawPrimitive(PrimitiveElement element,
     case PE_PanelLineEdit:
         if (const QStyleOptionFrame *panel = qstyleoption_cast<const QStyleOptionFrame *>(option)) {
             GtkWidget *gtkEntry = QGtk::gtkWidget(QLS("HildonEntry-finger"));
-            
+
             uint resolve_mask = option->palette.resolve();
-            QRect textRect = option->rect.adjusted(gtkEntry->style->xthickness, gtkEntry->style->ythickness,
-                                                   -gtkEntry->style->xthickness, -gtkEntry->style->ythickness);
 
-            gtkPainter.paintFlatBox(gtkEntry, "entry_bg", option->rect,
-                                    option->state & State_Enabled ? GTK_STATE_NORMAL : GTK_STATE_INSENSITIVE, GTK_SHADOW_NONE, gtkEntry->style);
+            //HildonEntry-finger could be ythickness = 0; We MUST adjust the height of the rect.
+            QRect textRect = option->rect.adjusted(gtkEntry->style->xthickness, 
+                                                   gtkEntry->style->ythickness ? gtkEntry->style->ythickness : gtkEntry->style->xthickness,
+                                                   -gtkEntry->style->xthickness, 
+                                                   -(gtkEntry->style->ythickness ? gtkEntry->style->ythickness : gtkEntry->style->xthickness) );
 
-            
-            drawPrimitive(PE_FrameLineEdit, option, painter, widget);
+            //Frame around LineEdits is drawn just for "pure" lineEdits (lineWidth > 0) and SpinBoxes
+            if (panel->lineWidth > 0 || QString::compare(widget->objectName(),"qt_spinbox_lineedit") == 0 ){
+                gtkPainter.paintFlatBox(gtkEntry, "entry_bg", option->rect,
+                                        option->state & State_Enabled ? GTK_STATE_NORMAL : GTK_STATE_INSENSITIVE, GTK_SHADOW_NONE, gtkEntry->style);
+                drawPrimitive(PE_FrameLineEdit, option, painter, widget);
+            }
 
         }
         break;
@@ -257,7 +276,6 @@ void QHildonStyle::drawPrimitive(PrimitiveElement element,
         if (!panel)
             break;
 
-        if (1){//panel->lineWidth > 0){
             //Draws a big frame
             GtkWidget *gtkEntry = QGtk::gtkWidget(QLS("HildonEntry-finger"));
 
@@ -279,27 +297,6 @@ void QHildonStyle::drawPrimitive(PrimitiveElement element,
                                    (option->state & State_HasFocus ? GTK_STATE_ACTIVE : GTK_STATE_NORMAL) : GTK_STATE_INSENSITIVE,
                                    GTK_SHADOW_IN, gtkEntry->style,
                                    option->state & State_HasFocus ? QLS("focus") : QString());
-        } else {
-            GtkWidget *gtkEntry = QGtk::gtkWidget(QLS("GtkEntry"));
-            gboolean interior_focus;
-            gint focus_line_width;
-            QRect rect = option->rect;
-            QGtk::gtk_widget_style_get(gtkEntry,
-                                   "interior-focus", &interior_focus,
-                                   "focus-line-width", &focus_line_width, NULL);
-
-            //See https://bugzilla.mozilla.org/show_bug.cgi?id=405421 for info about this hack
-            g_object_set_data(G_OBJECT(gtkEntry), "transparent-bg-hint", GINT_TO_POINTER(TRUE));
-
-            if (!interior_focus && option->state & State_HasFocus)
-                rect.adjust(focus_line_width, focus_line_width, -focus_line_width, -focus_line_width);
-
-       
-            gtkPainter.paintShadow(gtkEntry, "entry", rect, option->state & State_Enabled ? 
-                                   (option->state & State_HasFocus ? GTK_STATE_ACTIVE : GTK_STATE_NORMAL) : GTK_STATE_INSENSITIVE,
-                                   GTK_SHADOW_IN, gtkEntry->style,
-                                   option->state & State_HasFocus ? QLS("focus") : QString());
-        }
     }
     break;
     default:
@@ -341,7 +338,203 @@ void QHildonStyle::drawComplexControl(ComplexControl control, const QStyleOption
     QPalette palette = option->palette;
 
     switch (control) {
-    #ifndef QT_NO_SPINBOX
+    #ifndef QT_NO_COMBOBOX
+
+    case CC_ComboBox:
+        // See: http://live.gnome.org/GnomeArt/Tutorials/GtkThemes/GtkComboBox
+        // and http://live.gnome.org/GnomeArt/Tutorials/GtkThemes/GtkComboBoxEntry
+        if (const QStyleOptionComboBox *comboBox = qstyleoption_cast<const QStyleOptionComboBox *>(option)) {
+            bool sunken = comboBox->state & State_On; // play dead, if combobox has no items
+            BEGIN_STYLE_PIXMAPCACHE(QString::fromLatin1("cb-%0-%1").arg(sunken).arg(comboBox->editable));
+            QGtkPainter gtkCachedPainter(p);
+            gtkCachedPainter.setUsePixmapCache(false); // cached externally
+
+            bool isEnabled = (comboBox->state & State_Enabled);
+            bool focus = isEnabled && (comboBox->state & State_HasFocus);
+            QColor buttonShadow = option->palette.dark().color();
+            GtkStateType state = gtkPainter.gtkState(option);
+            int appears_as_list = !styleHint(QStyle::SH_ComboBox_Popup, comboBox, widget);
+            QPixmap cache;
+            QString pixmapName;
+            QStyleOptionComboBox comboBoxCopy = *comboBox;
+            comboBoxCopy.rect = option->rect;
+
+            bool reverse = (option->direction == Qt::RightToLeft);
+            QRect rect = option->rect;
+            QRect arrowButtonRect = subControlRect(CC_ComboBox, &comboBoxCopy,
+                                                   SC_ComboBoxArrow, widget);
+            QRect editRect = subControlRect(CC_ComboBox, &comboBoxCopy,
+                                            SC_ComboBoxEditField, widget);
+
+            GtkShadowType shadow = (option->state & State_Sunken || option->state & State_On ) ?
+                                   GTK_SHADOW_IN : GTK_SHADOW_OUT;
+            QString comboBoxPath = QLS(comboBox->editable ? "GtkComboBoxEntry" : "GtkComboBox");
+
+            // We use the gtk widget to position arrows and separators for us
+            GtkWidget *gtkCombo = QGtk::gtkWidget(comboBoxPath);
+            GtkAllocation geometry = {0, 0, option->rect.width(), option->rect.height()};
+            QGtk::gtk_widget_set_direction(gtkCombo, reverse ? GTK_TEXT_DIR_RTL : GTK_TEXT_DIR_LTR);
+            QGtk::gtk_widget_size_allocate(gtkCombo, &geometry);
+
+            QString buttonPath = comboBoxPath + QLS(".GtkToggleButton");
+            GtkWidget *gtkToggleButton = QGtk::gtkWidget(buttonPath);
+            QGtk::gtk_widget_set_direction(gtkToggleButton, reverse ? GTK_TEXT_DIR_RTL : GTK_TEXT_DIR_LTR);
+            if (gtkToggleButton && (appears_as_list || comboBox->editable)) {
+                if (focus)
+                    GTK_WIDGET_SET_FLAGS(gtkToggleButton, GTK_HAS_FOCUS);
+                // Draw the combo box as a line edit with a button next to it
+                if (comboBox->editable || appears_as_list) {
+                    GtkStateType frameState = (state == GTK_STATE_PRELIGHT) ? GTK_STATE_NORMAL : state;
+                    QString entryPath = QLS(comboBox->editable ? "GtkComboBoxEntry.GtkEntry" : "GtkComboBox.GtkFrame");
+                    GtkWidget *gtkEntry = QGtk::gtkWidget(entryPath);
+                    QGtk::gtk_widget_set_direction(gtkEntry, reverse ? GTK_TEXT_DIR_RTL : GTK_TEXT_DIR_LTR);
+                    QRect frameRect = option->rect;
+
+                    if (reverse)
+                        frameRect.setLeft(arrowButtonRect.right());
+                    else
+                        frameRect.setRight(arrowButtonRect.left());
+
+                    uint resolve_mask = option->palette.resolve();
+                    int xt = gtkEntry->style->xthickness;
+                    int yt = gtkEntry->style->ythickness;
+                    QRect contentRect = frameRect.adjusted(xt, yt, -xt, -yt);
+                                     
+                    if (comboBox->editable) {
+                        gtkCachedPainter.paintFlatBox(gtkEntry, "entry_bg", contentRect,
+                                                      option->state & State_Enabled ? GTK_STATE_ACTIVE : GTK_STATE_INSENSITIVE,
+                                                      GTK_SHADOW_NONE, gtkEntry->style, entryPath + QString::number(focus));
+                    } else {
+                        //Evil hack used by Gtkrc
+                        GdkColor gdkBg;
+                        QColor bg;
+                        gdkBg = gtkCombo->style->base[option->state & State_Enabled ? GTK_STATE_NORMAL : GTK_STATE_INSENSITIVE];
+                        bg = QColor(gdkBg.red>>8, gdkBg.green>>8, gdkBg.blue>>8);
+                        p->fillRect(contentRect, bg);
+                    }
+
+                    gtkCachedPainter.paintShadow(gtkEntry, comboBox->editable ? "entry" : "frame", frameRect, frameState,
+                                           GTK_SHADOW_IN, gtkEntry->style, entryPath +
+                                           QString::number(focus) + QString::number(comboBox->editable) +
+                                           QString::number(option->direction));
+                }
+
+                GtkStateType buttonState = GTK_STATE_NORMAL;
+
+                if (!(option->state & State_Enabled))
+                    buttonState = GTK_STATE_INSENSITIVE;
+                else if (option->state & State_Sunken || option->state & State_On)
+                    buttonState = GTK_STATE_ACTIVE;
+                else if (option->state & State_MouseOver && comboBox->activeSubControls & SC_ComboBoxArrow)
+                    buttonState = GTK_STATE_PRELIGHT;
+
+                QRect buttonrect = QRect(gtkToggleButton->allocation.x, gtkToggleButton->allocation.y,
+                                         gtkToggleButton->allocation.width, gtkToggleButton->allocation.height);
+
+                Q_ASSERT(gtkToggleButton);
+                gtkCachedPainter.paintBox( gtkToggleButton, "button", arrowButtonRect, buttonState,
+                                     shadow, gtkToggleButton->style, buttonPath +
+                                     QString::number(focus) + QString::number(option->direction));
+                if (focus)
+                    GTK_WIDGET_UNSET_FLAGS(gtkToggleButton, GTK_HAS_FOCUS);
+            } else {
+                // Draw combo box as a button
+                QRect buttonRect = option->rect;
+
+                if (focus) // Clearlooks actually check the widget for the default state
+                    GTK_WIDGET_SET_FLAGS(gtkToggleButton, GTK_HAS_FOCUS);
+                gtkCachedPainter.paintBox(gtkToggleButton, "button",
+                                    buttonRect, state,
+                                    shadow, gtkToggleButton->style,
+                                    buttonPath + QString::number(focus));
+                if (focus)
+                    GTK_WIDGET_UNSET_FLAGS(gtkToggleButton, GTK_HAS_FOCUS);
+
+                // Draw the separator between label and arrows
+                QString vSeparatorPath = buttonPath + QLS(".GtkHBox.GtkVSeparator");
+
+                if (GtkWidget *gtkVSeparator = QGtk::gtkWidget(vSeparatorPath)) {
+                    QRect vLineRect(gtkVSeparator->allocation.x,
+                                    gtkVSeparator->allocation.y,
+                                    gtkVSeparator->allocation.width,
+                                    gtkVSeparator->allocation.height);
+
+                    gtkCachedPainter.paintVline( gtkVSeparator, "vseparator",
+                                           vLineRect, state, gtkVSeparator->style,
+                                           0, vLineRect.height(), 0,  vSeparatorPath);
+
+
+                    gint interiorFocus = true;
+                    QGtk::gtk_widget_style_get(gtkToggleButton, "interior-focus", &interiorFocus, NULL);
+                    int xt = interiorFocus ? gtkToggleButton->style->xthickness : 0;
+                    int yt = interiorFocus ? gtkToggleButton->style->ythickness : 0;
+                    if ((focus && (option->state & State_KeyboardFocusChange)))
+                        gtkCachedPainter.paintFocus(gtkToggleButton, "button",
+                                              option->rect.adjusted(xt, yt, -xt, -yt),
+                                              option->state & State_Sunken ? GTK_STATE_ACTIVE : GTK_STATE_NORMAL,
+                                              gtkToggleButton->style);
+                }
+            }
+
+            if (comboBox->subControls & SC_ComboBoxArrow) {
+                if (!isEnabled)
+                    state = GTK_STATE_INSENSITIVE;
+                else if (sunken)
+                    state = GTK_STATE_ACTIVE;
+                else if (option->state & State_MouseOver)
+                    state = GTK_STATE_PRELIGHT;
+                else
+                    state = GTK_STATE_NORMAL;
+
+                QString arrowPath = comboBoxPath + QLS(appears_as_list ? ".GtkToggleButton.GtkArrow"
+                                                    : ".GtkToggleButton.GtkHBox.GtkArrow");
+
+                GtkWidget *gtkArrow = QGtk::gtkWidget(arrowPath);
+                gfloat scale = 0.7;
+                gint minSize = 15;
+                QRect arrowWidgetRect;
+
+                if (gtkArrow && !QGtk::gtk_check_version(2, 12, 0)) {
+                    QGtk::gtk_widget_style_get(gtkArrow, "arrow-scaling", &scale, NULL);
+                    QGtk::gtk_widget_style_get(gtkCombo, "arrow-size", &minSize, NULL);
+                }
+                if (gtkArrow) {
+                    arrowWidgetRect = QRect(gtkArrow->allocation.x, gtkArrow->allocation.y,
+                                            gtkArrow->allocation.width, gtkArrow->allocation.height);
+                    style = gtkArrow->style;
+                }
+
+                // Note that for some reason the arrow-size is not properly respected with Hildon
+                // Hence we enforce the minimum "arrow-size" ourselves
+                int arrowSize = qMax(qMin(rect.height() - gtkCombo->style->ythickness * 2, minSize),
+                                     qMin(arrowWidgetRect.width(), arrowWidgetRect.height()));
+                QRect arrowRect(0, 0, static_cast<int>(arrowSize * scale), static_cast<int>(arrowSize * scale));
+
+                arrowRect.moveCenter(arrowWidgetRect.center());
+
+                if (sunken) {
+                    int xoff, yoff;
+                    GtkWidget *gtkButton = QGtk::gtkWidget(comboBoxPath + QLS(".GtkToggleButton"));
+                    QGtk::gtk_widget_style_get(gtkButton, "child-displacement-x", &xoff, NULL);
+                    QGtk::gtk_widget_style_get(gtkButton, "child-displacement-y", &yoff, NULL);
+                    arrowRect = arrowRect.adjusted(xoff, yoff, xoff, yoff);
+                }
+
+                // Some styles such as Nimbus paint outside the arrowRect
+                // hence we have provide the whole widget as the cliprect
+                if (gtkArrow) {
+                    gtkCachedPainter.setClipRect(option->rect);
+                    gtkCachedPainter.paintArrow( gtkArrow, "arrow", arrowRect,
+                                           GTK_ARROW_DOWN, state, GTK_SHADOW_NONE, TRUE,
+                                           style, arrowPath + QString::number(option->direction));
+                }
+            }
+            END_STYLE_PIXMAPCACHE;
+        }
+        break;
+#endif // QT_NO_COMBOBOX
+
+#ifndef QT_NO_SPINBOX
     case CC_SpinBox:
         if (const QStyleOptionSpinBox *spinBox = qstyleoption_cast<const QStyleOptionSpinBox *>(option)) {
             GtkWidget *gtkSpinButton = QGtk::gtkWidget(QLS("GtkSpinButton"));
