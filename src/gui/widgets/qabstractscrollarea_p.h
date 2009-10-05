d6da3fcb0a42b29bdceb6b1a5b6f1807ead246df
Antonio Aloisio <antonio.aloisio@gmail.com>
2009-10-05 15:36:57 +0300

    Add QAbstractScrollAreaScroller class

--- src/gui/widgets/qabstractscrollarea_p.h
+++ src/gui/widgets/qabstractscrollarea_p.h
@@ -132,6 +132,78 @@ private:
     Qt::Orientation orientation;
 };
 
+#ifdef Q_WS_HILDON
+// This is the Kinetic fingerscroll implementation for Hildon
+#include <qqueue.h>
+#include <qdatetime.h>
+
+
+class QAbstractScrollAreaScroller : public QObject
+{
+    Q_OBJECT
+public:
+    QAbstractScrollAreaScroller(QWidget *parent, QAbstractScrollArea *qabstractscrollarea, QAbstractScrollAreaPrivate *priv);
+    bool eventFilter(QObject *obj, QEvent *event);
+
+protected Q_SLOTS:
+    void replayEvents();
+
+protected:
+    void handleMoveEvent ( QMouseEvent * event );
+    void timerEvent(QTimerEvent *event);
+    void registerChildrenForFingerScrolling(QObject *top);
+
+    static const int SENSITIVITY, 
+                     KINETIC_REFRESH,
+                     VSCALE,
+                     DECEL_DURATION,
+                     DEBOUNCE,
+                     DECEL_PC,
+                     START_WITHIN,
+                     VMAX,
+                     MAX_OVERSHOOT,
+                     OVERSHOOT_DECEL_PC,
+                     REBOUND_ACCEL;
+    
+    QAbstractScrollArea  *qabstractscrollarea;  // the area we're scrolling
+    QAbstractScrollAreaPrivate *qabstractscrollarea_d; // and its private data
+    
+    typedef enum { NotScrolling = 0,
+                   Maybe,
+                   ManualScroll,
+                   AutoScroll,
+                   OverShootDecel,
+                   OverShootPause,
+                   OverShootStabilise,
+                   ReissuingEvents } ScrollState;
+
+    ScrollState  scrollState,
+                 xScrollState,
+                 yScrollState;
+
+    QPointer<QWidget> eventSourceWidget; // who do we play events back to
+    QQueue<QEvent*>   storedEvents;      // events to play back
+
+    QPoint scrollBase;           // scrollbar value at fingerdown to establish base position
+    QPoint start;                // global event x,y at start
+    QPoint curr;                 // current x,y (kept to allow velocity calc)
+    QPoint rest;                 // rest x,y (resting place after overshoot
+
+    QPointF scrollFactor;        // scroll range factor - converts pixel delta to sb value delta
+
+    QTime event_time;
+    QPoint last;                 // global event x,y at start
+    QPoint vel;                  // Velocity
+    QPoint vel1;                 // Velocity before
+    int last_ev_time, curr_time; // timer for velocity
+        
+    int scrollingNotStartedTimer;
+    int x_braking_cycles;          // how long do we slow down for
+    int y_braking_cycles;          // how long do we slow down for
+    QPoint overshoot;            // overshoot x,y (distance past scrollarea limits)
+};
+#endif // Q_WS_HILDON
+
 #endif // QT_NO_SCROLLAREA
 
 QT_END_NAMESPACE
