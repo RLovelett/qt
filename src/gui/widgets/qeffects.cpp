25cc365e984228160296264e7dfe85f2db3b5959
miniak <milan.burda@gmail.com>
2010-04-06 19:26:22 +0200

    implement qFadeEffect() using QPropertyAnimation

--- src/gui/widgets/qeffects.cpp
+++ src/gui/widgets/qeffects.cpp
@@ -41,10 +41,9 @@
 
 #include "qapplication.h"
 #ifndef QT_NO_EFFECTS
-#include "qdesktopwidget.h"
 #include "qeffects_p.h"
+#include "qpropertyanimation.h"
 #include "qevent.h"
-#include "qimage.h"
 #include "qpainter.h"
 #include "qpixmap.h"
 #include "qpointer.h"
@@ -53,299 +52,6 @@
 #include "qdebug.h"
 
 QT_BEGIN_NAMESPACE
-
-/*
-  Internal class to get access to protected QWidget-members
-*/
-
-class QAccessWidget : public QWidget
-{
-    friend class QAlphaWidget;
-    friend class QRollEffect;
-public:
-    QAccessWidget(QWidget* parent=0, Qt::WindowFlags f = 0)
-        : QWidget(parent, f) {}
-};
-
-/*
-  Internal class QAlphaWidget.
-
-  The QAlphaWidget is shown while the animation lasts
-  and displays the pixmap resulting from the alpha blending.
-*/
-
-class QAlphaWidget: public QWidget, private QEffects
-{
-    Q_OBJECT
-public:
-    QAlphaWidget(QWidget* w, Qt::WindowFlags f = 0);
-    ~QAlphaWidget();
-
-    void run(int time);
-
-protected:
-    void paintEvent(QPaintEvent* e);
-    void closeEvent(QCloseEvent*);
-    void alphaBlend();
-    bool eventFilter(QObject *, QEvent *);
-
-protected slots:
-    void render();
-
-private:
-    QPixmap pm;
-    double alpha;
-    QImage backImage;
-    QImage frontImage;
-    QImage mixedImage;
-    QPointer<QAccessWidget> widget;
-    int duration;
-    int elapsed;
-    bool showWidget;
-    QTimer anim;
-    QElapsedTimer checkTime;
-    double windowOpacity;
-};
-
-static QAlphaWidget* q_blend = 0;
-
-/*
-  Constructs a QAlphaWidget.
-*/
-QAlphaWidget::QAlphaWidget(QWidget* w, Qt::WindowFlags f)
-    : QWidget(QApplication::desktop()->screen(QApplication::desktop()->screenNumber(w)), f)
-{
-#ifndef Q_WS_WIN
-    setEnabled(false);
-#endif
-    setAttribute(Qt::WA_NoSystemBackground, true);
-    widget = (QAccessWidget*)w;
-    windowOpacity = w->windowOpacity();
-    alpha = 0;
-}
-
-QAlphaWidget::~QAlphaWidget()
-{
-#if defined(Q_WS_WIN) && !defined(Q_WS_WINCE)
-    // Restore user-defined opacity value
-    if (widget)
-        widget->setWindowOpacity(windowOpacity);
-#endif
-}
-
-/*
-  \reimp
-*/
-void QAlphaWidget::paintEvent(QPaintEvent*)
-{
-    QPainter p(this);
-    p.drawPixmap(0, 0, pm);
-}
-
-/*
-  Starts the alphablending animation.
-  The animation will take about \a time ms
-*/
-void QAlphaWidget::run(int time)
-{
-    duration = time;
-
-    if (duration < 0)
-        duration = 150;
-
-    if (!widget)
-        return;
-
-    elapsed = 0;
-    checkTime.start();
-
-    showWidget = true;
-#if defined(Q_OS_WIN) && !defined(Q_OS_WINCE)
-    qApp->installEventFilter(this);
-    widget->setWindowOpacity(0.0);
-    widget->show();
-    connect(&anim, SIGNAL(timeout()), this, SLOT(render()));
-    anim.start(1);
-#else
-    //This is roughly equivalent to calling setVisible(true) without actually showing the widget
-    widget->setAttribute(Qt::WA_WState_ExplicitShowHide, true);
-    widget->setAttribute(Qt::WA_WState_Hidden, false);
-
-    qApp->installEventFilter(this);
-
-    move(widget->geometry().x(),widget->geometry().y());
-    resize(widget->size().width(), widget->size().height());
-
-    frontImage = QPixmap::grabWidget(widget).toImage();
-    backImage = QPixmap::grabWindow(QApplication::desktop()->winId(),
-                                widget->geometry().x(), widget->geometry().y(),
-                                widget->geometry().width(), widget->geometry().height()).toImage();
-
-    if (!backImage.isNull() && checkTime.elapsed() < duration / 2) {
-        mixedImage = backImage.copy();
-        pm = QPixmap::fromImage(mixedImage);
-        show();
-        setEnabled(false);
-
-        connect(&anim, SIGNAL(timeout()), this, SLOT(render()));
-        anim.start(1);
-    } else {
-       duration = 0;
-       render();
-    }
-#endif
-}
-
-/*
-  \reimp
-*/
-bool QAlphaWidget::eventFilter(QObject *o, QEvent *e)
-{
-    switch (e->type()) {
-    case QEvent::Move:
-	    if (o != widget)
-	        break;
-	    move(widget->geometry().x(),widget->geometry().y());
-	    update();
-	    break;
-    case QEvent::Hide:
-    case QEvent::Close:
-	    if (o != widget)
-	        break;
-    case QEvent::MouseButtonPress:
-	case QEvent::MouseButtonDblClick:
-	    showWidget = false;
-	    render();
-	    break;
-    case QEvent::KeyPress: {
-	        QKeyEvent *ke = (QKeyEvent*)e;
-            if (ke->key() == Qt::Key_Escape) {
-		        showWidget = false;
-            } else {
-		        duration = 0;
-            }
-	        render();
-	        break;
-	}
-    default:
-	    break;
-    }
-    return QWidget::eventFilter(o, e);
-}
-
-/*
-  \reimp
-*/
-void QAlphaWidget::closeEvent(QCloseEvent *e)
-{
-    e->accept();
-    if (!q_blend)
-        return;
-
-    showWidget = false;
-    render();
-
-    QWidget::closeEvent(e);
-}
-
-/*
-  Render alphablending for the time elapsed.
-
-  Show the blended widget and free all allocated source
-  if the blending is finished.
-*/
-void QAlphaWidget::render()
-{
-    int tempel = checkTime.elapsed();
-    if (elapsed >= tempel)
-        elapsed++;
-    else
-        elapsed = tempel;
-
-    if (duration != 0)
-        alpha = tempel / double(duration);
-    else
-        alpha = 1;
-
-#if defined(Q_OS_WIN) && !defined(Q_OS_WINCE)
-    if (alpha >= windowOpacity || !showWidget) {
-        anim.stop();
-        qApp->removeEventFilter(this);
-        widget->setWindowOpacity(windowOpacity);
-        q_blend = 0;
-        deleteLater();
-    } else {
-        widget->setWindowOpacity(alpha);
-    }
-#else
-    if (alpha >= 1 || !showWidget) {
-        anim.stop();
-        qApp->removeEventFilter(this);
-
-        if (widget) {
-            if (!showWidget) {
-#ifdef Q_WS_WIN
-                setEnabled(true);
-                setFocus();
-#endif // Q_WS_WIN
-                widget->hide();
-            } else {
-                //Since we are faking the visibility of the widget 
-                //we need to unset the hidden state on it before calling show
-                widget->setAttribute(Qt::WA_WState_Hidden, true);
-                widget->show();
-                lower();
-            }
-        }
-        q_blend = 0;
-        deleteLater();
-    } else {
-        alphaBlend();
-        pm = QPixmap::fromImage(mixedImage);
-        repaint();
-    }
-#endif // defined(Q_OS_WIN) && !defined(Q_OS_WINCE)
-}
-
-/*
-  Calculate an alphablended image.
-*/
-void QAlphaWidget::alphaBlend()
-{
-    const int a = qRound(alpha*256);
-    const int ia = 256 - a;
-
-    const int sw = frontImage.width();
-    const int sh = frontImage.height();
-    const int bpl = frontImage.bytesPerLine();
-    switch(frontImage.depth()) {
-    case 32:
-        {
-            uchar *mixed_data = mixedImage.bits();
-            const uchar *back_data = backImage.bits();
-            const uchar *front_data = frontImage.bits();
-
-            for (int sy = 0; sy < sh; sy++) {
-                quint32* mixed = (quint32*)mixed_data;
-                const quint32* back = (const quint32*)back_data;
-                const quint32* front = (const quint32*)front_data;
-                for (int sx = 0; sx < sw; sx++) {
-                    quint32 bp = back[sx];
-                    quint32 fp = front[sx];
-
-                    mixed[sx] =  qRgb((qRed(bp)*ia + qRed(fp)*a)>>8,
-                                      (qGreen(bp)*ia + qGreen(fp)*a)>>8,
-                                      (qBlue(bp)*ia + qBlue(fp)*a)>>8);
-                }
-                mixed_data += bpl;
-                back_data += bpl;
-                front_data += bpl;
-            }
-        }
-    default:
-        break;
-    }
-}
 
 /*
   Internal class QRollEffect
@@ -370,7 +76,7 @@ private slots:
     void scroll();
 
 private:
-    QPointer<QAccessWidget> widget;
+    QPointer<QWidget> widget;
 
     int currentHeight;
     int currentWidth;
@@ -401,7 +107,7 @@ QRollEffect::QRollEffect(QWidget* w, Qt::WindowFlags f, DirFlags orient)
     setEnabled(false);
 #endif
 
-    widget = (QAccessWidget*) w;
+    widget = w;
     Q_ASSERT(widget);
 
     setAttribute(Qt::WA_NoSystemBackground, true);
@@ -598,23 +304,25 @@ void qScrollEffect(QWidget* w, QEffects::DirFlags orient, int time)
 */
 void qFadeEffect(QWidget* w, int time)
 {
-    if (q_blend) {
-        q_blend->deleteLater();
-        q_blend = 0;
+    static QPointer<QPropertyAnimation> animation = 0;
+
+    if (animation) {
+        animation->stop();
+        animation = 0;
     }
 
     if (!w)
         return;
 
-    QApplication::sendPostedEvents(w, QEvent::Move);
-    QApplication::sendPostedEvents(w, QEvent::Resize);
+    w->setWindowOpacity(0.0);
+    w->show();
 
-    Qt::WindowFlags flags = Qt::ToolTip;
+    animation = new QPropertyAnimation(w, "windowOpacity");
 
-    // those can be popups - they would steal the focus, but are disabled
-    q_blend = new QAlphaWidget(w, flags);
-
-    q_blend->run(time);
+    animation->setDuration(time < 0 ? 150 : time);
+    animation->setStartValue(0.0);
+    animation->setEndValue(1.0);
+    animation->start(QAbstractAnimation::DeleteWhenStopped);
 }
 
 QT_END_NAMESPACE
