RLovelett/qt

commit c1431e6daa5f0e229c2e79a945f28d00d70b0978

Optimization: Avoid data copy when reading jpegs from memory
If the iodevice is actually a qbuffer, avoid read() and just give
libjpeg a pointer to the contained data directly.
Related to QTBUG-9095.

Reviewed-by: Kim

src/plugins/imageformats/jpeg/qjpeghandler.cpp
@@ -44,6 +44,7 @@
 #include <qimage.h>
 #include <qvariant.h>
 #include <qvector.h>
+#include <qbuffer.h>
 
 #include <stdio.h>      // jpeglib needs this to be pre-included
 #include <setjmp.h>
@@ -102,6 +103,7 @@ struct my_jpeg_source_mgr : public jpeg_source_mgr {
     // Nothing dynamic - cannot rely on destruction over longjump
     QIODevice *device;
     JOCTET buffer[max_buf];
+    const QBuffer *memDevice;
 
 public:
     my_jpeg_source_mgr(QIODevice *device);
@@ -117,10 +119,14 @@ static void qt_init_source(j_decompress_ptr)
 
 static boolean qt_fill_input_buffer(j_decompress_ptr cinfo)
 {
-    int num_read;
     my_jpeg_source_mgr* src = (my_jpeg_source_mgr*)cinfo->src;
+    if (src->memDevice) {
+        src->next_input_byte = (const JOCTET *)src->memDevice->data().constData();
+        src->bytes_in_buffer = (size_t)src->memDevice->data().size();
+        return true;
+    }
     src->next_input_byte = src->buffer;
-    num_read = src->device->read((char*)src->buffer, max_buf);
+    int num_read = src->device->read((char*)src->buffer, max_buf);
     if (num_read <= 0) {
         // Insert a fake EOI marker - as per jpeglib recommendation
         src->buffer[0] = (JOCTET) 0xFF;
@@ -147,7 +153,7 @@ static void qt_skip_input_data(j_decompress_ptr cinfo, long num_bytes)
      * any trouble anyway --- large skips are infrequent.
      */
     if (num_bytes > 0) {
-        while (num_bytes > (long) src->bytes_in_buffer) {
+        while (num_bytes > (long) src->bytes_in_buffer) {  // Should not happen in case of memDevice
             num_bytes -= (long) src->bytes_in_buffer;
             (void) qt_fill_input_buffer(cinfo);
             /* note we assume that qt_fill_input_buffer will never return false,
@@ -178,6 +184,7 @@ inline my_jpeg_source_mgr::my_jpeg_source_mgr(QIODevice *device)
     jpeg_source_mgr::resync_to_restart = jpeg_resync_to_restart;
     jpeg_source_mgr::term_source = qt_term_source;
     this->device = device;
+    memDevice = qobject_cast<QBuffer *>(device);
     bytes_in_buffer = 0;
     next_input_byte = buffer;
 }

commit b91df411fbd1efc587fe8729ee207388e36322fd

Fixed a QBuffer warning caused by a regression in qjpeg
Commit c1431e6d introduced an invalid QBuffer seek that generates a
warning when decoding JPEG from QBuffer. Also fixes the behavior of
seeking after the image data, and the behavior when the device is not
at position 0.

src/plugins/imageformats/jpeg/qjpeghandler.cpp
@@ -121,8 +121,8 @@ static boolean qt_fill_input_buffer(j_decompress_ptr cinfo)
 {
     my_jpeg_source_mgr* src = (my_jpeg_source_mgr*)cinfo->src;
     if (src->memDevice) {
-        src->next_input_byte = (const JOCTET *)src->memDevice->data().constData();
-        src->bytes_in_buffer = (size_t)src->memDevice->data().size();
+        src->next_input_byte = (const JOCTET *)(src->memDevice->data().constData() + src->memDevice->pos());
+        src->bytes_in_buffer = (size_t)(src->memDevice->data().size() - src->memDevice->pos());
         return true;
     }
     src->next_input_byte = src->buffer;
@@ -169,7 +169,13 @@ static void qt_term_source(j_decompress_ptr cinfo)
 {
     my_jpeg_source_mgr* src = (my_jpeg_source_mgr*)cinfo->src;
     if (!src->device->isSequential())
-        src->device->seek(src->device->pos() - src->bytes_in_buffer);
+    {
+        // read() isn't used for memDevice, so seek past everything that was used
+        if (src->memDevice)
+            src->device->seek(src->device->pos() + (src->memDevice->data().size() - src->memDevice->pos() - src->bytes_in_buffer));
+        else
+            src->device->seek(src->device->pos() - src->bytes_in_buffer);
+    }
 }
 
 #if defined(Q_C_CALLBACKS)