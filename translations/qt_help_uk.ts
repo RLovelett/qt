e9dd1fbfaf3f6d13d0f74bfde2e6ed21a20afd61
Victor Ostashevsky <ostash@ostash.kiev.ua>
2010-07-29 14:51:40 +0300

    Location information deleted.

--- translations/qt_help_uk.ts
+++ translations/qt_help_uk.ts
@@ -4,27 +4,22 @@
 <context>
     <name>QCLuceneResultWidget</name>
     <message>
-        <location filename="../tools/assistant/lib/qhelpsearchresultwidget.cpp" line="+110"/>
         <source>Search Results</source>
         <translation>Результати пошуку</translation>
     </message>
     <message>
-        <location line="+7"/>
         <source>Note:</source>
         <translation>Примітка:</translation>
     </message>
     <message>
-        <location line="+1"/>
         <source>The search results may not be complete since the documentation is still being indexed!</source>
         <translation>Результати пошуку можуть бути не повні, оскільки документація досі індексується!</translation>
     </message>
     <message>
-        <location line="+11"/>
         <source>Your search did not match any documents.</source>
         <translation>Ваш пошук не повернув результатів.</translation>
     </message>
     <message>
-        <location line="+4"/>
         <source>(The reason for this might be that the documentation is still being indexed.)</source>
         <translation>(Причиною цього може бути те, що документація досі індексується.)</translation>
     </message>
@@ -32,7 +27,6 @@
 <context>
     <name>QHelp</name>
     <message>
-        <location filename="../tools/assistant/lib/qhelp_global.cpp" line="+64"/>
         <source>Untitled</source>
         <translation>Без назви</translation>
     </message>
@@ -40,78 +34,62 @@
 <context>
     <name>QHelpCollectionHandler</name>
     <message>
-        <location filename="../tools/assistant/lib/qhelpcollectionhandler.cpp" line="+79"/>
         <source>The collection file &apos;%1&apos; is not set up yet!</source>
         <translation>Файл колекції &apos;%1&apos; ще не встановлено!</translation>
     </message>
     <message>
-        <location line="+23"/>
         <source>Cannot load sqlite database driver!</source>
         <translation>Неможливо завантажити драйвер бази даних sqlite!</translation>
     </message>
     <message>
-        <location line="+11"/>
-        <location line="+52"/>
         <source>Cannot open collection file: %1</source>
         <translation>Неможливо відкрити файл колекції: %1</translation>
     </message>
     <message>
-        <location line="-40"/>
         <source>Cannot create tables in file %1!</source>
         <translation>Неможливо створити таблиці в файлі %1!</translation>
     </message>
     <message>
-        <location line="+16"/>
         <source>The collection file &apos;%1&apos; already exists!</source>
         <translation>Файл колекції &apos;%1&apos; вже існує!</translation>
     </message>
     <message>
-        <location line="+6"/>
         <source>Cannot create directory: %1</source>
         <translation>Неможливо створити теку: %1</translation>
     </message>
     <message>
-        <location line="+26"/>
         <source>Cannot copy collection file: %1</source>
         <translation>Неможливо скопіювати файл колекції: %1</translation>
     </message>
     <message>
-        <location line="+119"/>
         <source>Unknown filter &apos;%1&apos;!</source>
         <translation>Невідомий фільтр &apos;%1&apos;!</translation>
     </message>
     <message>
-        <location line="+53"/>
         <source>Cannot register filter %1!</source>
         <translation>Неможливо зареєструвати фільтр %1!</translation>
     </message>
     <message>
-        <location line="+44"/>
         <source>Cannot open documentation file %1!</source>
         <translation>Неможливо відкрити файл документації %1!</translation>
     </message>
     <message>
-        <location line="+6"/>
         <source>Invalid documentation file &apos;%1&apos;!</source>
         <translation>Неправильний файл документації &apos;%1&apos;!</translation>
     </message>
     <message>
-        <location line="+34"/>
         <source>The namespace %1 was not registered!</source>
         <translation>Простір імен %1 не зареєстровано!</translation>
     </message>
     <message>
-        <location line="+120"/>
         <source>Namespace %1 already exists!</source>
         <translation>Простір імен %1 вже існує!</translation>
     </message>
     <message>
-        <location line="+13"/>
         <source>Cannot register namespace &apos;%1&apos;!</source>
         <translation>Неможливо зареєструвати простір імен &apos;%1&apos;!</translation>
     </message>
     <message>
-        <location line="+24"/>
         <source>Cannot open database &apos;%1&apos; to optimize!</source>
         <translation>Неможливо відкрити базу даних &apos;%1&apos; для оптимізації!</translation>
     </message>
@@ -119,7 +97,6 @@
 <context>
     <name>QHelpDBReader</name>
     <message>
-        <location filename="../tools/assistant/lib/qhelpdbreader.cpp" line="+98"/>
         <source>Cannot open database &apos;%1&apos; &apos;%2&apos;: %3</source>
         <extracomment>The placeholders are: %1 - The name of the database which cannot be opened %2 - The unique id for the connection %3 - The actual error string</extracomment>
         <translation>Не можу відкрити базу даних &apos;%1&apos; &apos;%2&apos;: %3</translation>
@@ -128,12 +105,10 @@
 <context>
     <name>QHelpEngineCore</name>
     <message>
-        <location filename="../tools/assistant/lib/qhelpenginecore.cpp" line="+122"/>
         <source>Cannot open documentation file %1: %2!</source>
         <translation>Не можу відкрити файл документації %1: %2!</translation>
     </message>
     <message>
-        <location line="+404"/>
         <source>The specified namespace does not exist!</source>
         <translation>Вказаний простір імен не існує!</translation>
     </message>
@@ -141,132 +116,106 @@
 <context>
     <name>QHelpGenerator</name>
     <message>
-        <location filename="../tools/assistant/lib/qhelpgenerator.cpp" line="+158"/>
         <source>Invalid help data!</source>
         <translation>Неправильні дані довідки!</translation>
     </message>
     <message>
-        <location line="+6"/>
         <source>No output file name specified!</source>
         <translation>Не вказане ім&apos;я вихідного файлу!</translation>
     </message>
     <message>
-        <location line="+7"/>
         <source>The file %1 cannot be overwritten!</source>
         <translation>Неможливо перезаписати файл %1!</translation>
     </message>
     <message>
-        <location line="+7"/>
         <source>Building up file structure...</source>
         <translation>Побудова структури файлу....</translation>
     </message>
     <message>
-        <location line="+11"/>
         <source>Cannot open data base file %1!</source>
         <translation>Неможливо відкрити файл бази даних %1!</translation>
     </message>
     <message>
-        <location line="+14"/>
         <source>Cannot register namespace %1!</source>
         <translation>Неможливо зареєструвати простір імен %1!</translation>
     </message>
     <message>
-        <location line="+6"/>
         <source>Insert custom filters...</source>
         <translation>Вставка фільтрів користувача...</translation>
     </message>
     <message>
-        <location line="+12"/>
         <source>Insert help data for filter section (%1 of %2)...</source>
         <translation>Вставка даних довідки для розділу фільтра (%1 з %2)...</translation>
     </message>
     <message>
-        <location line="+18"/>
         <source>Documentation successfully generated.</source>
         <translation>Документацію успішно згенеровано.</translation>
     </message>
     <message>
-        <location line="+76"/>
         <source>Some tables already exist!</source>
         <translation>Деякі таблиці вже існують!</translation>
     </message>
     <message>
-        <location line="+61"/>
         <source>Cannot create tables!</source>
         <translation>Неможливо створити таблиці!</translation>
     </message>
     <message>
-        <location line="+86"/>
         <source>Cannot register virtual folder!</source>
         <translation>Неможливо зареєструвати віртуальну теку!</translation>
     </message>
     <message>
-        <location line="+10"/>
         <source>Insert files...</source>
         <translation>Вставка файлів...</translation>
     </message>
     <message>
-        <location line="+42"/>
         <source>The referenced file %1 must be inside or within a subdirectory of (%2). Skipping it.</source>
         <translation>Файл %1 має бути всередині підтеки (%2). Пропускаємо його.</translation>
     </message>
     <message>
-        <location line="+7"/>
         <source>The file %1 does not exist! Skipping it.</source>
         <translation>Файл %1 не існує! Пропускаємо його.</translation>
     </message>
     <message>
-        <location line="+6"/>
         <source>Cannot open file %1! Skipping it.</source>
         <translation>Неможливо відкрити файл %1! Пропускаємо його.</translation>
     </message>
     <message>
-        <location line="+133"/>
         <source>The filter %1 is already registered!</source>
         <translation>Фільтр %1 вже зареєстровано!</translation>
     </message>
     <message>
-        <location line="+5"/>
         <source>Cannot register filter %1!</source>
         <translation>Неможливо зареєструвати фільтр %1!</translation>
     </message>
     <message>
-        <location line="+24"/>
         <source>Insert indices...</source>
         <translation>Вставка індексів...</translation>
     </message>
     <message>
-        <location line="+91"/>
         <source>Insert contents...</source>
         <translation>Вставка змісту...</translation>
     </message>
     <message>
-        <location line="+8"/>
         <source>Cannot insert contents!</source>
         <translation>Неможливо вставити зміст!</translation>
     </message>
     <message>
-        <location line="+12"/>
         <source>Cannot register contents!</source>
         <translation>Неможливо зареєструвати зміст!</translation>
     </message>
     <message>
-        <location line="+56"/>
         <source>File &apos;%1&apos; does not exist.</source>
         <translation>Файл &apos;%1&apos; не існує.</translation>
     </message>
     <message>
-        <location line="+19"/>
         <source>File &apos;%1&apos; cannot be opened.</source>
         <translation>Неможливо відкрити файл &apos;%1&apos;.</translation>
     </message>
     <message>
-        <location line="+19"/>
         <source>File &apos;%1&apos; contains an invalid link to file &apos;%2&apos;</source>
         <translation>Файл &apos;%1&apos; містить неправильне посилання до файлу &apos;%2&apos;</translation>
     </message>
     <message>
-        <location line="+9"/>
         <source>Invalid links in HTML files.</source>
         <translation>Неправильні посилання в файлах HTML.</translation>
     </message>
@@ -274,47 +223,38 @@
 <context>
     <name>QHelpProject</name>
     <message>
-        <location filename="../tools/assistant/lib/qhelpprojectdata.cpp" line="+88"/>
         <source>Unknown token.</source>
         <translation>Невідомий токен.</translation>
     </message>
     <message>
-        <location line="+13"/>
         <source>Unknown token. Expected &quot;QtHelpProject&quot;!</source>
         <translation>Невідомий токен. Очікувався &quot;QtHelpProject&quot;!</translation>
     </message>
     <message>
-        <location line="+6"/>
         <source>Error in line %1: %2</source>
         <translation>Помилка в рядку %1: %2</translation>
     </message>
     <message>
-        <location line="+14"/>
         <source>Virtual folder has invalid syntax.</source>
         <translation>Віртуальна тека має неправильний синтаксис.</translation>
     </message>
     <message>
-        <location line="+5"/>
         <source>Namespace has invalid syntax.</source>
         <translation>Простір імен має неправильний синтаксис.</translation>
     </message>
     <message>
-        <location line="+19"/>
         <source>Missing namespace in QtHelpProject.</source>
         <translation>Відсутній простір імен в QtHelpProject.</translation>
     </message>
     <message>
-        <location line="+3"/>
         <source>Missing virtual folder in QtHelpProject</source>
         <translation>Відсутня віртуальна тека в QtHelpProject</translation>
     </message>
     <message>
-        <location line="+89"/>
         <source>Missing attribute in keyword at line %1.</source>
         <translation>Відсутній атрибут в ключовому слові на рядку %1.</translation>
     </message>
     <message>
-        <location line="+143"/>
         <source>The input file %1 could not be opened!</source>
         <translation>Неможливо відкрити вхідний файл %1!</translation>
     </message>
@@ -322,52 +262,42 @@
 <context>
     <name>QHelpSearchQueryWidget</name>
     <message>
-        <location filename="../tools/assistant/lib/qhelpsearchquerywidget.cpp" line="+124"/>
         <source>Search for:</source>
         <translation>Шукати:</translation>
     </message>
     <message>
-        <location line="+1"/>
         <source>Previous search</source>
         <translation>Попередній пошук</translation>
     </message>
     <message>
-        <location line="+1"/>
         <source>Next search</source>
         <translation>Наступний пошук</translation>
     </message>
     <message>
-        <location line="+1"/>
         <source>Search</source>
         <translation>Шукати</translation>
     </message>
     <message>
-        <location line="+2"/>
         <source>Advanced search</source>
         <translation>Розширений пошук</translation>
     </message>
     <message>
-        <location line="+1"/>
         <source>words &lt;B&gt;similar&lt;/B&gt; to:</source>
         <translation>слова &lt;B&gt;схожі&lt;/B&gt; на:</translation>
     </message>
     <message>
-        <location line="+1"/>
         <source>&lt;B&gt;without&lt;/B&gt; the words:</source>
         <translation>&lt;B&gt;без&lt;/B&gt; слів:</translation>
     </message>
     <message>
-        <location line="+1"/>
         <source>with &lt;B&gt;exact phrase&lt;/B&gt;:</source>
         <translation>з &lt;B&gt;точною фразою&lt;/B&gt;:</translation>
     </message>
     <message>
-        <location line="+1"/>
         <source>with &lt;B&gt;all&lt;/B&gt; of the words:</source>
         <translation>з &lt;B&gt;усіма&lt;/B&gt; словами:</translation>
     </message>
     <message>
-        <location line="+1"/>
         <source>with &lt;B&gt;at least one&lt;/B&gt; of the words:</source>
         <translation>з &lt;B&gt;щонайменше одним&lt;/B&gt; зі слів:</translation>
     </message>
@@ -375,7 +305,6 @@
 <context>
     <name>QHelpSearchResultWidget</name>
     <message numerus="yes">
-        <location filename="../tools/assistant/lib/qhelpsearchresultwidget.cpp" line="+174"/>
         <source>%1 - %2 of %n Hits</source>
         <translation>
             <numerusform>%1 - %2 з %n співпадіння</numerusform>
@@ -384,7 +313,6 @@
         </translation>
     </message>
     <message>
-        <location line="+61"/>
         <source>0 - 0 of 0 Hits</source>
         <translation>0 - 0 з 0 співпадінь</translation>
     </message>
