RLovelett/qt

commit fb7d86cf23227302d48db279ec589221d11a1f6a

Move QLineEdit logic into QLineControl
Manually merged from the kinetic repository, where it has been used
for a while. Like QTextControl, there is no auto test for the control,
just for the widgets built with it.

The purpose of this change is so that we can build stuff like QLineEdit
in other places (like kinetic). Also included is a trivial change to
QValidator, needed to use it with QLineControl in QML.

Reviewed-by: mbm

src/gui/widgets/qlineedit.h
@@ -268,6 +268,8 @@ private:
     Q_DECLARE_PRIVATE(QLineEdit)
     Q_PRIVATE_SLOT(d_func(), void _q_handleWindowActivate())
     Q_PRIVATE_SLOT(d_func(), void _q_deleteSelected())
+    Q_PRIVATE_SLOT(d_func(), void _q_textEdited(const QString &))
+    Q_PRIVATE_SLOT(d_func(), void _q_cursorPositionChanged(int, int))
 #ifndef QT_NO_COMPLETER
     Q_PRIVATE_SLOT(d_func(), void _q_completionHighlighted(QString))
 #endif

src/gui/widgets/qlineedit_p.cpp
@@ -1,0 +1,253 @@
+/****************************************************************************
+**
+** Copyright (C) 2009 Nokia Corporation and/or its subsidiary(-ies).
+** Contact: Qt Software Information (qt-info@nokia.com)
+**
+** This file is part of the QtGui module of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:LGPL$
+** No Commercial Usage
+** This file contains pre-release code and may not be distributed.
+** You may use this file in accordance with the terms and conditions
+** contained in the either Technology Preview License Agreement or the
+** Beta Release License Agreement.
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Nokia gives you certain
+** additional rights. These rights are described in the Nokia Qt LGPL
+** Exception version 1.0, included in the file LGPL_EXCEPTION.txt in this
+** package.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 3.0 as published by the Free Software
+** Foundation and appearing in the file LICENSE.GPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU General Public License version 3.0 requirements will be
+** met: http://www.gnu.org/copyleft/gpl.html.
+**
+** If you are unsure which license is appropriate for your use, please
+** contact the sales department at qt-sales@nokia.com.
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+#include "qlineedit.h"
+#include "qlineedit_p.h"
+
+#ifndef QT_NO_LINEEDIT
+
+#include "qabstractitemview.h"
+#include "qclipboard.h"
+#ifndef QT_NO_ACCESSIBILITY
+#include "qaccessible.h"
+#endif
+#ifndef QT_NO_IM
+#include "qinputcontext.h"
+#include "qlist.h"
+#endif
+
+QT_BEGIN_NAMESPACE
+
+const int QLineEditPrivate::verticalMargin(1);
+const int QLineEditPrivate::horizontalMargin(2);
+
+int QLineEditPrivate::xToPos(int x, QTextLine::CursorPosition betweenOrOn) const
+{
+    QRect cr = adjustedContentsRect();
+    x-= cr.x() - hscroll + horizontalMargin;
+    return control->xToPos(x, betweenOrOn);
+}
+
+QRect QLineEditPrivate::cursorRect() const
+{
+    QRect cr = adjustedContentsRect();
+    int cix = cr.x() - hscroll + horizontalMargin;
+    QRect crect = control->cursorRect();
+    crect.moveTo(crect.topLeft() + QPoint(cix, vscroll));
+    return crect;
+}
+
+#ifndef QT_NO_COMPLETER
+
+void QLineEditPrivate::_q_completionHighlighted(QString newText)
+{
+    Q_Q(QLineEdit);
+    if (control->completer()->completionMode() != QCompleter::InlineCompletion) {
+        q->setText(newText);
+    } else {
+        int c = control->cursor();
+        QString text = control->text();
+        q->setText(text.left(c) + newText.mid(c));
+        control->moveCursor(control->end(), false);
+        control->moveCursor(c, true);
+    }
+}
+
+#endif // QT_NO_COMPLETER
+
+void QLineEditPrivate::_q_clipboardChanged()
+{
+}
+
+void QLineEditPrivate::_q_handleWindowActivate()
+{
+    Q_Q(QLineEdit);
+    if (!q->hasFocus() && control->hasSelectedText())
+        control->deselect();
+}
+
+void QLineEditPrivate::_q_deleteSelected()
+{
+}
+
+void QLineEditPrivate::_q_textEdited(const QString &text)
+{
+    Q_Q(QLineEdit);
+#ifndef QT_NO_COMPLETER
+    if (control->completer() &&
+            control->completer()->completionMode() != QCompleter::InlineCompletion)
+        control->complete(-1); // update the popup on cut/paste/del
+#endif
+    emit q->textEdited(text);
+}
+
+void QLineEditPrivate::_q_cursorPositionChanged(int from, int to)
+{
+    Q_Q(QLineEdit);
+    q->update();
+    emit q->cursorPositionChanged(from, to);
+}
+
+void QLineEditPrivate::init(const QString& txt)
+{
+    Q_Q(QLineEdit);
+    control = new QLineControl(txt);
+    QObject::connect(control, SIGNAL(textChanged(const QString &)),
+            q, SIGNAL(textChanged(const QString &)));
+    QObject::connect(control, SIGNAL(textEdited(const QString &)),
+            q, SLOT(_q_textEdited(const QString &)));
+    QObject::connect(control, SIGNAL(cursorPositionChanged(int, int)),
+            q, SLOT(_q_cursorPositionChanged(int, int)));
+    QObject::connect(control, SIGNAL(selectionChanged()),
+            q, SIGNAL(selectionChanged()));
+    QObject::connect(control, SIGNAL(accepted()),
+            q, SIGNAL(returnPressed()));
+    QObject::connect(control, SIGNAL(editingFinished()),
+            q, SIGNAL(editingFinished()));
+
+    // for now, going completely overboard with updates.
+    QObject::connect(control, SIGNAL(selectionChanged()),
+            q, SLOT(update()));
+
+    QObject::connect(control, SIGNAL(displayTextChanged(const QString &)),
+            q, SLOT(update()));
+    control->setPasswordCharacter(q->style()->styleHint(QStyle::SH_LineEdit_PasswordCharacter));
+#ifndef QT_NO_CURSOR
+    q->setCursor(Qt::IBeamCursor);
+#endif
+    q->setFocusPolicy(Qt::StrongFocus);
+    q->setAttribute(Qt::WA_InputMethodEnabled);
+    //   Specifies that this widget can use more, but is able to survive on
+    //   less, horizontal space; and is fixed vertically.
+    q->setSizePolicy(QSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed, QSizePolicy::LineEdit));
+    q->setBackgroundRole(QPalette::Base);
+    q->setAttribute(Qt::WA_KeyCompression);
+    q->setMouseTracking(true);
+    q->setAcceptDrops(true);
+
+    q->setAttribute(Qt::WA_MacShowFocusRect);
+}
+
+QRect QLineEditPrivate::adjustedContentsRect() const
+{
+    Q_Q(const QLineEdit);
+    QStyleOptionFrameV2 opt;
+    q->initStyleOption(&opt);
+    QRect r = q->style()->subElementRect(QStyle::SE_LineEditContents, &opt, q);
+    r.setX(r.x() + leftTextMargin);
+    r.setY(r.y() + topTextMargin);
+    r.setRight(r.right() - rightTextMargin);
+    r.setBottom(r.bottom() - bottomTextMargin);
+    return r;
+}
+
+void QLineEditPrivate::setCursorVisible(bool visible)
+{
+    Q_Q(QLineEdit);
+    if ((bool)cursorVisible == visible)
+        return;
+    cursorVisible = visible;
+    QRect r = cursorRect();
+    if (control->inputMask().isEmpty())
+        q->update(r);
+    else
+        q->update();
+}
+
+void QLineEditPrivate::updatePasswordEchoEditing(bool editing)
+{
+    Q_Q(QLineEdit);
+    control->updatePasswordEchoEditing(editing);
+    q->setAttribute(Qt::WA_InputMethodEnabled, shouldEnableInputMethod());
+}
+
+/*!
+  This function is not intended as polymorphic usage. Just a shared code
+  fragment that calls QInputContext::mouseHandler for this
+  class.
+*/
+bool QLineEditPrivate::sendMouseEventToInputContext( QMouseEvent *e )
+{
+#if !defined QT_NO_IM
+    Q_Q(QLineEdit);
+    if ( control->composeMode() ) {
+	int tmp_cursor = xToPos(e->pos().x());
+	int mousePos = tmp_cursor - control->cursor();
+	if ( mousePos < 0 || mousePos > control->preeditAreaText().length() ) {
+            mousePos = -1;
+	    // don't send move events outside the preedit area
+            if ( e->type() == QEvent::MouseMove )
+                return true;
+        }
+
+        QInputContext *qic = q->inputContext();
+        if ( qic )
+            // may be causing reset() in some input methods
+            qic->mouseHandler(mousePos, e);
+        if (!control->preeditAreaText().isEmpty())
+            return true;
+    }
+#else
+    Q_UNUSED(e);
+#endif
+
+    return false;
+}
+
+#ifndef QT_NO_DRAGANDDROP
+void QLineEditPrivate::drag()
+{
+    Q_Q(QLineEdit);
+    dndTimer.stop();
+    QMimeData *data = new QMimeData;
+    data->setText(control->selectedText());
+    QDrag *drag = new QDrag(q);
+    drag->setMimeData(data);
+    Qt::DropAction action = drag->start();
+    if (action == Qt::MoveAction && !control->isReadOnly() && drag->target() != q)
+        control->removeSelection();
+}
+
+#endif // QT_NO_DRAGANDDROP
+
+QT_END_NAMESPACE
+
+#endif

src/gui/widgets/qlineedit_p.h
@@ -65,6 +65,8 @@
 #include "QtCore/qpointer.h"
 #include "QtGui/qlineedit.h"
 
+#include "private/qlinecontrol_p.h"
+
 QT_BEGIN_NAMESPACE
 
 class QLineEditPrivate : public QWidgetPrivate
@@ -73,167 +75,64 @@ class QLineEditPrivate : public QWidgetPrivate
 public:
 
     QLineEditPrivate()
-        : cursor(0), preeditCursor(0), cursorTimer(0), frame(1),
-          cursorVisible(0), hideCursor(false), separator(0), readOnly(0),
-          dragEnabled(0), contextMenuEnabled(1), echoMode(0), textDirty(0),
-          selDirty(0), validInput(1), alignment(Qt::AlignLeading | Qt::AlignVCenter), ascent(0),
-          maxLength(32767), hscroll(0), vscroll(0), lastCursorPos(-1), maskData(0),
-          modifiedState(0), undoState(0), selstart(0), selend(0), userInput(false),
-          emitingEditingFinished(false), passwordEchoEditing(false)
-#ifndef QT_NO_COMPLETER
-        , completer(0)
-#endif
-        , leftTextMargin(0), topTextMargin(0), rightTextMargin(0), bottomTextMargin(0)
-        {
-        }
+        : control(0), frame(1), contextMenuEnabled(1), cursorVisible(0),
+        dragEnabled(0), hscroll(0), vscroll(0),
+        alignment(Qt::AlignLeading | Qt::AlignVCenter),
+        leftTextMargin(0), topTextMargin(0), rightTextMargin(0), bottomTextMargin(0)
+    {
+    }
 
     ~QLineEditPrivate()
     {
-        delete [] maskData;
     }
-    void init(const QString&);
 
-    QString text;
-    int cursor;
-    int preeditCursor;
-    int cursorTimer; // -1 for non blinking cursor.
-    QPoint tripleClick;
-    QBasicTimer tripleClickTimer;
-    uint frame : 1;
-    uint cursorVisible : 1;
-    uint hideCursor : 1; // used to hide the cursor inside preedit areas
-    uint separator : 1;
-    uint readOnly : 1;
-    uint dragEnabled : 1;
-    uint contextMenuEnabled : 1;
-    uint echoMode : 2;
-    uint textDirty : 1;
-    uint selDirty : 1;
-    uint validInput : 1;
-    uint alignment;
-    int ascent;
-    int maxLength;
-    int hscroll;
-    int vscroll;
-    int lastCursorPos;
+    QLineControl *control;
 
 #ifndef QT_NO_CONTEXTMENU
     QPointer<QAction> selectAllAction;
 #endif
+    void init(const QString&);
 
-    inline void emitCursorPositionChanged();
-    bool sendMouseEventToInputContext(QMouseEvent *e);
-
-    void finishChange(int validateFromState = -1, bool update = false, bool edited = true);
-
-    QPointer<QValidator> validator;
-    struct MaskInputData {
-        enum Casemode { NoCaseMode, Upper, Lower };
-        QChar maskChar; // either the separator char or the inputmask
-        bool separator;
-        Casemode caseMode;
-    };
-    QString inputMask;
-    QChar blank;
-    MaskInputData *maskData;
-    inline int nextMaskBlank(int pos) {
-        int c = findInMask(pos, true, false);
-        separator |= (c != pos);
-        return (c != -1 ?  c : maxLength);
-    }
-    inline int prevMaskBlank(int pos) {
-        int c = findInMask(pos, false, false);
-        separator |= (c != pos);
-        return (c != -1 ? c : 0);
-    }
-
-    void setCursorVisible(bool visible);
-
-
-    // undo/redo handling
-    enum CommandType { Separator, Insert, Remove, Delete, RemoveSelection, DeleteSelection, SetSelection };
-    struct Command {
-        inline Command() {}
-        inline Command(CommandType t, int p, QChar c, int ss, int se) : type(t),uc(c),pos(p),selStart(ss),selEnd(se) {}
-        uint type : 4;
-        QChar uc;
-        int pos, selStart, selEnd;
-    };
-    int modifiedState;
-    int undoState;
-    QVector<Command> history;
-    void addCommand(const Command& cmd);
-    void insert(const QString& s);
-    void del(bool wasBackspace = false);
-    void remove(int pos);
-
-    inline void separate() { separator = true; }
-    void undo(int until = -1);
-    void redo();
-    inline bool isUndoAvailable() const { return !readOnly && undoState; }
-    inline bool isRedoAvailable() const { return !readOnly && undoState < (int)history.size(); }
-
-    // selection
-    int selstart, selend;
-    inline bool allSelected() const { return !text.isEmpty() && selstart == 0 && selend == (int)text.length(); }
-    inline bool hasSelectedText() const { return !text.isEmpty() && selend > selstart; }
-    inline void deselect() { selDirty |= (selend > selstart); selstart = selend = 0; }
-    void removeSelectedText();
-#ifndef QT_NO_CLIPBOARD
-    void copy(bool clipboard = true) const;
-#endif
-    inline bool inSelection(int x) const
-    { if (selstart >= selend) return false;
-    int pos = xToPos(x, QTextLine::CursorOnCharacter);  return pos >= selstart && pos < selend; }
-
-    // masking
-    void parseInputMask(const QString &maskFields);
-    bool isValidInput(QChar key, QChar mask) const;
-    bool hasAcceptableInput(const QString &text) const;
-    QString maskString(uint pos, const QString &str, bool clear = false) const;
-    QString clearString(uint pos, uint len) const;
-    QString stripString(const QString &str) const;
-    int findInMask(int pos, bool forward, bool findSeparator, QChar searchChar = QChar()) const;
-
-    // input methods
-    bool composeMode() const { return !textLayout.preeditAreaText().isEmpty(); }
-
-    // complex text layout
-    QTextLayout textLayout;
-    void updateTextLayout();
-    void moveCursor(int pos, bool mark = false);
-    void setText(const QString& txt, int pos = -1, bool edited = true);
     int xToPos(int x, QTextLine::CursorPosition = QTextLine::CursorBetweenCharacters) const;
     QRect cursorRect() const;
-    bool fixup();
+    void setCursorVisible(bool visible);
+
+    void updatePasswordEchoEditing(bool);
+
+    inline bool shouldEnableInputMethod() const
+    {
+        return !control->isReadOnly() && (control->echoMode() == QLineEdit::Normal || control->echoMode() == QLineEdit::PasswordEchoOnEdit);
+    }
+
+    QPoint tripleClick;
+    QBasicTimer tripleClickTimer;
+    uint frame : 1;
+    uint contextMenuEnabled : 1;
+    uint cursorVisible : 1;
+    uint dragEnabled : 1;
+    int hscroll;
+    int vscroll;
+    uint alignment;
+    static const int verticalMargin;
+    static const int horizontalMargin;
+
+    bool sendMouseEventToInputContext(QMouseEvent *e);
 
     QRect adjustedContentsRect() const;
 
+    void _q_clipboardChanged();
+    void _q_handleWindowActivate();
+    void _q_deleteSelected();
+    void _q_textEdited(const QString &);
+    void _q_cursorPositionChanged(int, int);
+
+#ifndef QT_NO_COMPLETER
+    void _q_completionHighlighted(QString);
+#endif
 #ifndef QT_NO_DRAGANDDROP
-    // drag and drop
     QPoint dndPos;
     QBasicTimer dndTimer;
     void drag();
-#endif
-
-    void _q_handleWindowActivate();
-    void _q_deleteSelected();
-    bool userInput;
-    bool emitingEditingFinished;
-
-#ifdef QT_KEYPAD_NAVIGATION
-    QBasicTimer deleteAllTimer; // keypad navigation
-    QString origText;
-#endif
-
-    bool passwordEchoEditing;
-    void updatePasswordEchoEditing(bool editing);
-
-#ifndef QT_NO_COMPLETER
-    QPointer<QCompleter> completer;
-    void complete(int key = -1);
-    void _q_completionHighlighted(QString);
-    bool advanceToEnabledItem(int n);
 #endif
 
     int leftTextMargin;

src/gui/widgets/qabstractspinbox.cpp
@@ -976,7 +976,7 @@ void QAbstractSpinBox::keyPressEvent(QKeyEvent *event)
 #endif
     case Qt::Key_Enter:
     case Qt::Key_Return:
-        d->edit->d_func()->modifiedState = d->edit->d_func()->undoState = 0;
+        d->edit->d_func()->control->clearUndo();
         d->interpret(d->keyboardTracking ? AlwaysEmit : EmitIfChanged);
         selectAll();
         event->ignore();

commit e4e92df09e234be1739a830926c1baed8d2c74bd

Fix partial updates for blinking cursor in QLineEdit.
Regression introduced by change fb7d86cf and the whole refactoring
of QLineEdit into QLineControl.

Task-number: QTBUG-12709

src/gui/widgets/qlineedit.cpp
@@ -1856,36 +1856,21 @@ void QLineEdit::paintEvent(QPaintEvent *)
     QPainter p(this);
 
     QRect r = rect();
+    QRect lineRect;
+    QPoint topLeft;
     QPalette pal = palette();
 
     QStyleOptionFrameV2 panel;
     initStyleOption(&panel);
     style()->drawPrimitive(QStyle::PE_PanelLineEdit, &panel, &p, this);
-    r = style()->subElementRect(QStyle::SE_LineEditContents, &panel, this);
-    r.setX(r.x() + d->leftTextMargin);
-    r.setY(r.y() + d->topTextMargin);
-    r.setRight(r.right() - d->rightTextMargin);
-    r.setBottom(r.bottom() - d->bottomTextMargin);
-    p.setClipRect(r);
 
-    QFontMetrics fm = fontMetrics();
-    Qt::Alignment va = QStyle::visualAlignment(layoutDirection(), QFlag(d->alignment));
-    switch (va & Qt::AlignVertical_Mask) {
-     case Qt::AlignBottom:
-         d->vscroll = r.y() + r.height() - fm.height() - d->verticalMargin;
-         break;
-     case Qt::AlignTop:
-         d->vscroll = r.y() + d->verticalMargin;
-         break;
-     default:
-         //center
-         d->vscroll = r.y() + (r.height() - fm.height() + 1) / 2;
-         break;
-    }
-    QRect lineRect(r.x() + d->horizontalMargin, d->vscroll, r.width() - 2*d->horizontalMargin, fm.height());
+    d->updateScroll(&r, &lineRect, &topLeft);
+
+    p.setClipRect(r);
 
     if (d->control->text().isEmpty()) {
         if (!hasFocus() && !d->placeholderText.isEmpty()) {
+            Qt::Alignment va = QStyle::visualAlignment(layoutDirection(), QFlag(d->alignment));
             QColor col = pal.text().color();
             col.setAlpha(128);
             QPen oldpen = p.pen();
@@ -1895,46 +1880,6 @@ void QLineEdit::paintEvent(QPaintEvent *)
             return;
         }
     }
-
-    int cix = qRound(d->control->cursorToX());
-
-    // horizontal scrolling. d->hscroll is the left indent from the beginning
-    // of the text line to the left edge of lineRect. we update this value
-    // depending on the delta from the last paint event; in effect this means
-    // the below code handles all scrolling based on the textline (widthUsed,
-    // minLB, minRB), the line edit rect (lineRect) and the cursor position
-    // (cix).
-    int minLB = qMax(0, -fm.minLeftBearing());
-    int minRB = qMax(0, -fm.minRightBearing());
-    int widthUsed = qRound(d->control->naturalTextWidth()) + 1 + minRB;
-    if ((minLB + widthUsed) <=  lineRect.width()) {
-        // text fits in lineRect; use hscroll for alignment
-        switch (va & ~(Qt::AlignAbsolute|Qt::AlignVertical_Mask)) {
-        case Qt::AlignRight:
-            d->hscroll = widthUsed - lineRect.width() + 1;
-            break;
-        case Qt::AlignHCenter:
-            d->hscroll = (widthUsed - lineRect.width()) / 2;
-            break;
-        default:
-            // Left
-            d->hscroll = 0;
-            break;
-        }
-        d->hscroll -= minLB;
-    } else if (cix - d->hscroll >= lineRect.width()) {
-        // text doesn't fit, cursor is to the right of lineRect (scroll right)
-        d->hscroll = cix - lineRect.width() + 1;
-    } else if (cix - d->hscroll < 0 && d->hscroll < widthUsed) {
-        // text doesn't fit, cursor is to the left of lineRect (scroll left)
-        d->hscroll = cix;
-    } else if (widthUsed - d->hscroll < lineRect.width()) {
-        // text doesn't fit, text document is to the left of lineRect; align
-        // right
-        d->hscroll = widthUsed - lineRect.width() + 1;
-    }
-    // the y offset is there to keep the baseline constant in case we have script changes in the text.
-    QPoint topLeft = lineRect.topLeft() - QPoint(d->hscroll, d->control->ascent() - fm.ascent());
 
     // draw text, selections and cursors
 #ifndef QT_NO_STYLE_STYLESHEET

src/gui/widgets/qlineedit.h
@@ -280,6 +280,7 @@ private:
     Q_DISABLE_COPY(QLineEdit)
     Q_DECLARE_PRIVATE(QLineEdit)
     Q_PRIVATE_SLOT(d_func(), void _q_handleWindowActivate())
+    Q_PRIVATE_SLOT(d_func(), void _q_updateControlRect(const QRect &))
     Q_PRIVATE_SLOT(d_func(), void _q_textEdited(const QString &))
     Q_PRIVATE_SLOT(d_func(), void _q_cursorPositionChanged(int, int))
 #ifndef QT_NO_COMPLETER

src/gui/widgets/qlineedit_p.cpp
@@ -100,6 +100,14 @@ void QLineEditPrivate::_q_handleWindowActivate()
         control->deselect();
 }
 
+void QLineEditPrivate::_q_updateControlRect(const QRect &rect)
+{
+    Q_Q(QLineEdit);
+    QPoint topLeft;
+    updateScroll(0, 0, &topLeft);
+    q->update(rect.translated(topLeft));
+}
+
 void QLineEditPrivate::_q_textEdited(const QString &text)
 {
     Q_Q(QLineEdit);
@@ -176,7 +184,7 @@ void QLineEditPrivate::init(const QString& txt)
             q, SLOT(update()));
 
     QObject::connect(control, SIGNAL(updateNeeded(QRect)),
-            q, SLOT(update()));
+            q, SLOT(_q_updateControlRect(QRect)));
 
     QStyleOptionFrameV2 opt;
     q->initStyleOption(&opt);
@@ -208,6 +216,82 @@ QRect QLineEditPrivate::adjustedContentsRect() const
     r.setRight(r.right() - rightTextMargin);
     r.setBottom(r.bottom() - bottomTextMargin);
     return r;
+}
+
+void QLineEditPrivate::updateScroll(QRect *rect, QRect *lineRect, QPoint *topLeft)
+{
+    Q_Q(const QLineEdit);
+    QPalette pal = q->palette();
+
+    QStyleOptionFrameV2 panel;
+    q->initStyleOption(&panel);
+    QRect r = q->style()->subElementRect(QStyle::SE_LineEditContents, &panel, q);
+    r.setX(r.x() + leftTextMargin);
+    r.setY(r.y() + topTextMargin);
+    r.setRight(r.right() - rightTextMargin);
+    r.setBottom(r.bottom() - bottomTextMargin);
+
+    QFontMetrics fm = q->fontMetrics();
+    Qt::Alignment va = QStyle::visualAlignment(q->layoutDirection(), QFlag(alignment));
+    switch (va & Qt::AlignVertical_Mask) {
+     case Qt::AlignBottom:
+         vscroll = r.y() + r.height() - fm.height() - verticalMargin;
+         break;
+     case Qt::AlignTop:
+         vscroll = r.y() + verticalMargin;
+         break;
+     default:
+         //center
+         vscroll = r.y() + (r.height() - fm.height() + 1) / 2;
+         break;
+    }
+    QRect lr(r.x() + horizontalMargin, vscroll, r.width() - 2*horizontalMargin, fm.height());
+
+    int cix = qRound(control->cursorToX());
+
+    // horizontal scrolling. hscroll is the left indent from the beginning
+    // of the text line to the left edge of lineRect (lr). we update this value
+    // depending on the delta from the last paint event; in effect this means
+    // the below code handles all scrolling based on the textline (widthUsed,
+    // minLB, minRB), the line edit rect (lr) and the cursor position
+    // (cix).
+    int minLB = qMax(0, -fm.minLeftBearing());
+    int minRB = qMax(0, -fm.minRightBearing());
+    int widthUsed = qRound(control->naturalTextWidth()) + 1 + minRB;
+    if ((minLB + widthUsed) <=  lr.width()) {
+        // text fits in lineRect; use hscroll for alignment
+        switch (va & ~(Qt::AlignAbsolute|Qt::AlignVertical_Mask)) {
+        case Qt::AlignRight:
+            hscroll = widthUsed - lr.width() + 1;
+            break;
+        case Qt::AlignHCenter:
+            hscroll = (widthUsed - lr.width()) / 2;
+            break;
+        default:
+            // Left
+            hscroll = 0;
+            break;
+        }
+        hscroll -= minLB;
+    } else if (cix - hscroll >= lr.width()) {
+        // text doesn't fit, cursor is to the right of lr (scroll right)
+        hscroll = cix - lr.width() + 1;
+    } else if (cix - hscroll < 0 && hscroll < widthUsed) {
+        // text doesn't fit, cursor is to the left of lr (scroll left)
+        hscroll = cix;
+    } else if (widthUsed - hscroll < lr.width()) {
+        // text doesn't fit, text document is to the left of lr; align
+        // right
+        hscroll = widthUsed - lr.width() + 1;
+    }
+
+    if (rect)
+        *rect = r;
+    if (lineRect)
+        *lineRect = lr;
+    // the y offset is there to keep the baseline constant in case we have script changes in the text.
+    if (topLeft)
+        *topLeft = lr.topLeft() - QPoint(hscroll, control->ascent() - fm.ascent());
 }
 
 void QLineEditPrivate::setCursorVisible(bool visible)

src/gui/widgets/qlineedit_p.h
@@ -121,8 +121,10 @@ public:
     bool sendMouseEventToInputContext(QMouseEvent *e);
 
     QRect adjustedContentsRect() const;
+    void updateScroll(QRect *rect, QRect *lineRect, QPoint *topLeft);
 
     void _q_handleWindowActivate();
+    void _q_updateControlRect(const QRect &rect);
     void _q_textEdited(const QString &);
     void _q_cursorPositionChanged(int, int);
 #ifdef QT_KEYPAD_NAVIGATION

tests/auto/qlineedit/tst_qlineedit.cpp
@@ -272,6 +272,7 @@ private slots:
     void taskQTBUG_4679_moveToStartEndOfBlock();
     void taskQTBUG_4679_selectToStartEndOfBlock();
     void taskQTBUG_7395_readOnlyShortcut();
+    void taskQTBUG_12709_lineEditPartialUpdates();
 
 #ifdef QT3_SUPPORT
     void validateAndSet_data();
@@ -3689,6 +3690,32 @@ void tst_QLineEdit::taskQTBUG_7395_readOnlyShortcut()
 
     QTest::keyClick(0, Qt::Key_P);
     QCOMPARE(spy.count(), 1);
+}
+
+class LineEditRepaintRecorder : public QLineEdit
+{
+public:
+    QList<QRegion> repaintRegions;
+
+protected:
+    void paintEvent(QPaintEvent *event)
+    {
+        repaintRegions << event->region();
+    }
+};
+
+void tst_QLineEdit::taskQTBUG_12709_lineEditPartialUpdates()
+{
+    LineEditRepaintRecorder le;
+    le.show();
+    QTest::qWaitForWindowShown(&le);
+    QApplication::setActiveWindow(&le);
+    le.setFocus();
+    QTRY_VERIFY(le.hasFocus());
+
+    QTest::qWait(1000);
+    QVERIFY(!le.repaintRegions.isEmpty());
+    QVERIFY(le.repaintRegions.last().boundingRect().width() < le.width() / 4);
 }
 
 QTEST_MAIN(tst_QLineEdit)