6f39d18d8056a1137c34069c48dca2af266e958c
Konstantin Ritt <ritt.ks@gmail.com>
2010-11-05 18:29:43 +0300

    optimize canonicalOrderHelper() for generic case
    if there is no need to swap codepoints A and B,
    then we proceeding with advance to the next codepoint,
    and this next codepoint B becomes to A, and the next-next codepoint C becomes to B;
    in such case we can easily skip the re-calculations for A
    by using previously calculated results for B

--- src/corelib/tools/qchar.cpp
+++ src/corelib/tools/qchar.cpp
@@ -1562,10 +1562,14 @@ static void canonicalOrderHelper(QString *str, QChar::UnicodeVersion version, in
 {
     QString &s = *str;
     const int l = s.length()-1;
+
+    uint u1, u2;
+    ushort c1, c2;
+
     int pos = from;
     while (pos < l) {
         int p2 = pos+1;
-        uint u1 = s.at(pos).unicode();
+        u1 = s.at(pos).unicode();
         if (QChar(u1).isHighSurrogate()) {
             ushort low = s.at(p2).unicode();
             if (QChar(low).isLowSurrogate()) {
@@ -1575,7 +1579,10 @@ static void canonicalOrderHelper(QString *str, QChar::UnicodeVersion version, in
                 ++p2;
             }
         }
-        uint u2 = s.at(p2).unicode();
+        c1 = 0;
+
+    advance:
+        u2 = s.at(p2).unicode();
         if (QChar(u2).isHighSurrogate() && p2 < l) {
             ushort low = s.at(p2+1).unicode();
             if (QChar(low).isLowSurrogate()) {
@@ -1583,8 +1590,7 @@ static void canonicalOrderHelper(QString *str, QChar::UnicodeVersion version, in
                 ++p2;
             }
         }
-
-        ushort c2 = 0;
+        c2 = 0;
         {
             const QUnicodeTables::Properties *p = qGetProp(u2);
             if ((QChar::UnicodeVersion)p->unicodeVersion <= version)
@@ -1595,8 +1601,7 @@ static void canonicalOrderHelper(QString *str, QChar::UnicodeVersion version, in
             continue;
         }
 
-        ushort c1 = 0;
-        {
+        if (c1 == 0) {
             const QUnicodeTables::Properties *p = qGetProp(u1);
             if ((QChar::UnicodeVersion)p->unicodeVersion <= version)
                 c1 = p->combiningClass;
@@ -1626,6 +1631,16 @@ static void canonicalOrderHelper(QString *str, QChar::UnicodeVersion version, in
             ++pos;
             if (QChar::requiresSurrogates(u1))
                 ++pos;
+
+            u1 = u2;
+            c1 = c2; // != 0
+            p2 = pos + 1;
+            if (QChar::requiresSurrogates(u1))
+                ++p2;
+            if (p2 > l)
+                break;
+
+            goto advance;
         }
     }
 }
