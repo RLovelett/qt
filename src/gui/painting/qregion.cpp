01634963dd85a2f5fdf91843db31bf3a0a37a69b
Niklas Kurkisuo <ext-niklas.kurkisuo@nokia.com>
2010-11-09 08:33:18 +0200

    Fix resource leak in PolygonRegion.
    CID 22046.

--- src/gui/painting/qregion.cpp
+++ src/gui/painting/qregion.cpp
@@ -3602,8 +3602,10 @@ static QRegionPrivate *PolygonRegion(const QPoint *Pts, int Count, int rule)
         return region;
     }
 
-    if (!(pETEs = static_cast<EdgeTableEntry *>(malloc(sizeof(EdgeTableEntry) * Count))))
+    if (!(pETEs = static_cast<EdgeTableEntry *>(malloc(sizeof(EdgeTableEntry) * Count)))) {
+        delete region;
         return 0;
+    }
 
     region->vectorize();
 
