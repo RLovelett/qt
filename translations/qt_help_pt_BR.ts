147e25adc2d581aafb96e2a7f511a7fa7e38808f
boaglio <boaglio@gmail.com>
2010-11-16 00:05:11 -0200

    initial import for pt_BR translation

---
+++ translations/qt_help_pt_BR.ts
@@ -0,0 +1,318 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>QCLuceneResultWidget</name>
+    <message>
+        <source>Search Results</source>
+        <translation>Busca por resultados</translation>
+    </message>
+    <message>
+        <source>Note:</source>
+        <translation>Nota</translation>
+    </message>
+    <message>
+        <source>The search results may not be complete since the documentation is still being indexed!</source>
+        <translation>Os resultados da busca podem não estar completos porque o indexação da documentação está em andamento!</translation>
+    </message>
+    <message>
+        <source>Your search did not match any documents.</source>
+        <translation>Sua busca não retornou nenhum resultado</translation>
+    </message>
+    <message>
+        <source>(The reason for this might be that the documentation is still being indexed.)</source>
+        <translation>(isso pode estar ocorrendo porque a indexação da documentação está em andamento.)</translation>
+    </message>
+</context>
+<context>
+    <name>QHelp</name>
+    <message>
+        <source>Untitled</source>
+        <translation>Sem título</translation>
+    </message>
+</context>
+<context>
+    <name>QHelpCollectionHandler</name>
+    <message>
+        <source>The collection file &apos;%1&apos; is not set up yet!</source>
+        <translation>O arquivo de catálogo &apos;%1&apos; não está configurado!</translation>
+    </message>
+    <message>
+        <source>Cannot load sqlite database driver!</source>
+        <translation>Não foi possível carregar o driver do banco de dados sqlite!</translation>
+    </message>
+    <message>
+        <source>Cannot open collection file: %1</source>
+        <translation>Não foi possível abrir o arquivo de catálogo: %1</translation>
+    </message>
+    <message>
+        <source>Cannot create tables in file %1!</source>
+        <translation>Não foi possível criar as tabelas no arquivo %1!</translation>
+    </message>
+    <message>
+        <source>The collection file &apos;%1&apos; already exists!</source>
+        <translation>O arquivo de catálogo &apos;%1&apos; já existe !</translation>
+    </message>
+    <message>
+        <source>Cannot create directory: %1</source>
+        <translation>Não foi possível criar o diretório: %1</translation>
+    </message>
+    <message>
+        <source>Cannot copy collection file: %1</source>
+        <translation>Não foi possível copiar o arquivo de catálogo: %1</translation>
+    </message>
+    <message>
+        <source>Unknown filter &apos;%1&apos;!</source>
+        <translation>Filtro desconhecido: &apos;%1&apos;!</translation>
+    </message>
+    <message>
+        <source>Cannot register filter %1!</source>
+        <translation>Não foi possível registrar o filtro %1!</translation>
+    </message>
+    <message>
+        <source>Cannot open documentation file %1!</source>
+        <translation>Não foi possível abrir o arquivo de documentação %1!</translation>
+    </message>
+    <message>
+        <source>Invalid documentation file &apos;%1&apos;!</source>
+        <translation>Arquivo de documentação inválido: &apos;%1&apos;!</translation>
+    </message>
+    <message>
+        <source>The namespace %1 was not registered!</source>
+        <translation>O namespace %1 não foi registrado!</translation>
+    </message>
+    <message>
+        <source>Namespace %1 already exists!</source>
+        <translation>O namespace %1 já existe!</translation>
+    </message>
+    <message>
+        <source>Cannot register namespace &apos;%1&apos;!</source>
+        <translation>Não foi possível registrar o namespace &apos;%1&apos;!</translation>
+    </message>
+    <message>
+        <source>Cannot open database &apos;%1&apos; to optimize!</source>
+        <translation>Não foi possível abrir o banco de dados  &apos;%1&apos; para otimização!</translation>
+    </message>
+</context>
+<context>
+    <name>QHelpDBReader</name>
+    <message>
+        <source>Cannot open database &apos;%1&apos; &apos;%2&apos;: %3</source>
+        <extracomment>The placeholders are: %1 - The name of the database which cannot be opened %2 - The unique id for the connection %3 - The actual error string</extracomment>
+        <translation>Não foi possível abrir o banco de dados &apos;%1&apos; com a conexão &apos;%2&apos;, mensagem de erro: %3</translation>
+    </message>
+</context>
+<context>
+    <name>QHelpEngineCore</name>
+    <message>
+        <source>Cannot open documentation file %1: %2!</source>
+        <translation>Não foi possível abrir o arquivo de documentação %1: %2! </translation>
+    </message>
+    <message>
+        <source>The specified namespace does not exist!</source>
+        <translation>O namespace especificado não existe!</translation>
+    </message>
+</context>
+<context>
+    <name>QHelpGenerator</name>
+    <message>
+        <source>Invalid help data!</source>
+        <translation>Dados de ajuda inválidos!</translation>
+    </message>
+    <message>
+        <source>No output file name specified!</source>
+        <translation>Nenhum arquivo de saída foi especificado!</translation>
+    </message>
+    <message>
+        <source>The file %1 cannot be overwritten!</source>
+        <translation>O arquivo %1 não pode ser sobrescrito!</translation>
+    </message>
+    <message>
+        <source>Building up file structure...</source>
+        <translation>Construindo a estrutura de arquivos...</translation>
+    </message>
+    <message>
+        <source>Cannot open data base file %1!</source>
+        <translation>Não foi possível abrir o arquivo de banco de dados %1!</translation>
+    </message>
+    <message>
+        <source>Cannot register namespace %1!</source>
+        <translation>Não foi possível registrar o namespace %1!</translation>
+    </message>
+    <message>
+        <source>Insert custom filters...</source>
+        <translation>Adicionar filtros customizados...</translation>
+    </message>
+    <message>
+        <source>Insert help data for filter section (%1 of %2)...</source>
+        <translation>Adicionar dados de ajuda para a seção de filtros (%1 de %2)...</translation>
+    </message>
+    <message>
+        <source>Documentation successfully generated.</source>
+        <translation>Documentação gerada com sucesso.</translation>
+    </message>
+    <message>
+        <source>Some tables already exist!</source>
+        <translation>Algumas tabelas já existem!</translation>
+    </message>
+    <message>
+        <source>Cannot create tables!</source>
+        <translation>Não foi possível criar as tabelas!</translation>
+    </message>
+    <message>
+        <source>Cannot register virtual folder!</source>
+        <translation>Não foi possível registrar a pasta virtual!</translation>
+    </message>
+    <message>
+        <source>Insert files...</source>
+        <translation>Adicionar arquivos...</translation>
+    </message>
+    <message>
+        <source>The referenced file %1 must be inside or within a subdirectory of (%2). Skipping it.</source>
+        <translation>O arquivo referenciado %1 deve estar dentro do subdiretório de (%2). Ignorado.</translation>
+    </message>
+    <message>
+        <source>The file %1 does not exist! Skipping it.</source>
+        <translation>O arquivo %1 não existe! Ignorado.</translation>
+    </message>
+    <message>
+        <source>Cannot open file %1! Skipping it.</source>
+        <translation>Não foi possível abrir o arquivo %1! Ignorado.</translation>
+    </message>
+    <message>
+        <source>The filter %1 is already registered!</source>
+        <translation>O filtro %1 já está registrado!</translation>
+    </message>
+    <message>
+        <source>Cannot register filter %1!</source>
+        <translation>Não foi possível registrar o filtro %1!</translation>
+    </message>
+    <message>
+        <source>Insert indices...</source>
+        <translation>Adicionar índices...</translation>
+    </message>
+    <message>
+        <source>Insert contents...</source>
+        <translation>Adicionar conteúdo...</translation>
+    </message>
+    <message>
+        <source>Cannot insert contents!</source>
+        <translation>Não foi possível adicionar conteúdo!</translation>
+    </message>
+    <message>
+        <source>Cannot register contents!</source>
+        <translation>Não foi possível registrar o conteúdo!</translation>
+    </message>
+    <message>
+        <source>File &apos;%1&apos; does not exist.</source>
+        <translation>O arquivo &apos;%1&apos; não existe.</translation>
+    </message>
+    <message>
+        <source>File &apos;%1&apos; cannot be opened.</source>
+        <translation>O arquivo &apos;%1&apos; não pode ser aberto.</translation>
+    </message>
+    <message>
+        <source>File &apos;%1&apos; contains an invalid link to file &apos;%2&apos;</source>
+        <translation>O arquivo &apos;%1&apos; contém um link inválido para o arquivo &apos;%2&apos;</translation>
+    </message>
+    <message>
+        <source>Invalid links in HTML files.</source>
+        <translation>Existem links inválidos para os arquivos HTML.</translation>
+    </message>
+</context>
+<context>
+    <name>QHelpProject</name>
+    <message>
+        <source>Unknown token.</source>
+        <translation>Token desconhecido.</translation>
+    </message>
+    <message>
+        <source>Unknown token. Expected &quot;QtHelpProject&quot;!</source>
+        <translation>Token desconhecido, um &quot;QtHelpProject&quot; era esperado!</translation>
+    </message>
+    <message>
+        <source>Error in line %1: %2</source>
+        <translation>Erro na linha %1: %2</translation>
+    </message>
+    <message>
+        <source>Virtual folder has invalid syntax.</source>
+        <translation>A pasta virtual está com a sintaxe inválida.</translation>
+    </message>
+    <message>
+        <source>Namespace has invalid syntax.</source>
+        <translation>O namespace está com a sintaxe inválida.</translation>
+    </message>
+    <message>
+        <source>Missing namespace in QtHelpProject.</source>
+        <translation>Namespace não encontrado em QtHelpProject.</translation>
+    </message>
+    <message>
+        <source>Missing virtual folder in QtHelpProject</source>
+        <translation>Pasta virtual não encontrada em QtHelpProject</translation>
+    </message>
+    <message>
+        <source>Missing attribute in keyword at line %1.</source>
+        <translation>Atributo não encontrado na linha %1.</translation>
+    </message>
+    <message>
+        <source>The input file %1 could not be opened!</source>
+        <translation>O arquivo %1 não pode ser aberto!</translation>
+    </message>
+</context>
+<context>
+    <name>QHelpSearchQueryWidget</name>
+    <message>
+        <source>Search for:</source>
+        <translation>Busca por:</translation>
+    </message>
+    <message>
+        <source>Previous search</source>
+        <translation>Busca anterior</translation>
+    </message>
+    <message>
+        <source>Next search</source>
+        <translation>Próxima busca</translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation>Busca</translation>
+    </message>
+    <message>
+        <source>Advanced search</source>
+        <translation>Busca avançada</translation>
+    </message>
+    <message>
+        <source>words &lt;B&gt;similar&lt;/B&gt; to:</source>
+        <translation>palavras &lt;B&gt;parecidas&lt;/B&gt; com:</translation>
+    </message>
+    <message>
+        <source>&lt;B&gt;without&lt;/B&gt; the words:</source>
+        <translation>&lt;B&gt;sem&lt;/B&gt; as palavras:</translation>
+    </message>
+    <message>
+        <source>with &lt;B&gt;exact phrase&lt;/B&gt;:</source>
+        <translation>com &lt;B&gt;a frase exata&lt;/B&gt;:</translation>
+    </message>
+    <message>
+        <source>with &lt;B&gt;all&lt;/B&gt; of the words:</source>
+        <translation>com &lt;B&gt;todas&lt;/B&gt; as palavras:</translation>
+    </message>
+    <message>
+        <source>with &lt;B&gt;at least one&lt;/B&gt; of the words:</source>
+        <translation>com &lt;B&gt;pelo menos&lt;/B&gt; uma das palavras:</translation>
+    </message>
+</context>
+<context>
+    <name>QHelpSearchResultWidget</name>
+    <message numerus="yes">
+        <source>%1 - %2 of %n Hits</source>
+        <translation>
+            <numerusform>%1 - 2% de %n resultados</numerusform>
+        </translation>
+    </message>
+    <message>
+        <source>0 - 0 of 0 Hits</source>
+        <translation>0 - 0 de 0 resultados</translation>
+    </message>
+</context>
+</TS>
---
+++ translations/qvfb_pt_BR.ts
@@ -0,0 +1,415 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>AnimationSaveWidget</name>
+    <message>
+        <source>Record</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Reset</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Save in MPEG format (requires netpbm package installed)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Click record to begin recording.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Finished saving.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Paused. Click record to resume, or save if done.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Pause</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Recording...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Saving... </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Save animation...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Save canceled.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Save failed!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Config</name>
+    <message>
+        <source>Configure</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>176x220 &quot;SmartPhone&quot;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>240x320 &quot;PDA&quot;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>320x240 &quot;TV&quot; / &quot;QVGA&quot;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>640x480 &quot;VGA&quot;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>800x480</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>800x600</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>1024x768</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Custom</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Depth</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>1 bit monochrome</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>2 bit grayscale</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>4 bit grayscale</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>8 bit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>12 (16) bit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>15 bit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>16 bit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>18 bit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>24 bit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>32 bit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>32 bit ARGB</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Swap red and blue channels</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>BGR format</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Skin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>None</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Emulate touch screen (no mouse move)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Emulate LCD screen (Only with fixed zoom of 3.0 times magnification)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&lt;p&gt;Note that any applications using the virtual framebuffer will be terminated if you change the Size or Depth &lt;i&gt;above&lt;/i&gt;. You may freely modify the Gamma &lt;i&gt;below&lt;/i&gt;.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Gamma</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Blue</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>1.0</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Green</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>All</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Red</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Set all to 1.0</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DeviceSkin</name>
+    <message>
+        <source>The image file &apos;%1&apos; could not be loaded.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The skin directory &apos;%1&apos; does not contain a configuration file.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The skin configuration file &apos;%1&apos; could not be opened.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The skin configuration file &apos;%1&apos; could not be read: %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Syntax error: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The skin &quot;up&quot; image file &apos;%1&apos; does not exist.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The skin &quot;down&quot; image file &apos;%1&apos; does not exist.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The skin &quot;closed&quot; image file &apos;%1&apos; does not exist.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The skin cursor image file &apos;%1&apos; does not exist.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Syntax error in area definition: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Mismatch in number of areas, expected %1, got %2.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QVFb</name>
+    <message>
+        <source>&amp;File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Configure...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Save image...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Animation...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;View</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Show &amp;Cursor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Refresh Rate...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;No rotation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;90° rotation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>1&amp;80° rotation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>2&amp;70° rotation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Zoom scale &amp;0.5</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Zoom scale 0.7&amp;5</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Zoom scale &amp;1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Zoom scale &amp;2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Zoom scale &amp;3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Zoom scale &amp;4</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Zoom &amp;scale...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;About...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Save Main Screen image</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>snapshot.png</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Portable Network Graphics (*.png)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Save Main Screen Image</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Save failed. Check that you have permission to write to the target directory.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Save Second Screen image</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Save Second Screen Image</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>About QVFB</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&lt;h2&gt;The Qt for Embedded Linux Virtual X11 Framebuffer&lt;/h2&gt;&lt;p&gt;This application runs under Qt for X11, emulating a simple framebuffer, which the Qt for Embedded Linux server and clients can attach to just as if it was a hardware Linux framebuffer. &lt;p&gt;With the aid of this development tool, you can develop Qt for Embedded  Linux applications under X11 without having to switch to a virtual console. This means you can comfortably use your other development tools such as GUI profilers and debuggers.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Browse...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Load Custom Skin...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>All QVFB Skins (*.skin)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QVFbRateDialog</name>
+    <message>
+        <source>Target frame rate:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>%1fps</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
