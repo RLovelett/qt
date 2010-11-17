75dd4554721c6913d5af00c3794efed45640bdd8
Park Shinjo <peremen@gmail.com>
2010-11-17 12:18:50 +0900

    Updating Korean messages, based on discussions.

---
+++ translations/qt_help_ko.ts
@@ -0,0 +1,390 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ko_KR">
+<context>
+    <name>QCLuceneResultWidget</name>
+    <message>
+        <location filename="../tools/assistant/lib/qhelpsearchresultwidget.cpp" line="+110"/>
+        <source>Search Results</source>
+        <translation>검색 결과</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Note:</source>
+        <translation>메모:</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>The search results may not be complete since the documentation is still being indexed!</source>
+        <translation>문서의 인덱싱 작업이 진행 중이므로 검색 결과가 완전하지 않을 수도 있습니다!</translation>
+    </message>
+    <message>
+        <location line="+11"/>
+        <source>Your search did not match any documents.</source>
+        <translation>검색 결과가 없습니다.</translation>
+    </message>
+    <message>
+        <location line="+4"/>
+        <source>(The reason for this might be that the documentation is still being indexed.)</source>
+        <translation>(문서를 현재 인덱싱 중이므로 결과가 없을 수도 있습니다.)</translation>
+    </message>
+</context>
+<context>
+    <name>QHelp</name>
+    <message>
+        <location filename="../tools/assistant/lib/qhelp_global.cpp" line="+64"/>
+        <source>Untitled</source>
+        <translation>제목 없음</translation>
+    </message>
+</context>
+<context>
+    <name>QHelpCollectionHandler</name>
+    <message>
+        <location filename="../tools/assistant/lib/qhelpcollectionhandler.cpp" line="+79"/>
+        <source>The collection file &apos;%1&apos; is not set up yet!</source>
+        <translation>모음집 파일 &apos;%1&apos;이(가) 설정되지 않았습니다!</translation>
+    </message>
+    <message>
+        <location line="+23"/>
+        <source>Cannot load sqlite database driver!</source>
+        <translation>sqlite 데이터베이스 드라이버를 불러올 수 없습니다!</translation>
+    </message>
+    <message>
+        <location line="+11"/>
+        <location line="+52"/>
+        <source>Cannot open collection file: %1</source>
+        <translation>모음집 파일을 열 수 없음: %1</translation>
+    </message>
+    <message>
+        <location line="-40"/>
+        <source>Cannot create tables in file %1!</source>
+        <translation>파일 %1에 테이블을 만들 수 없습니다!</translation>
+    </message>
+    <message>
+        <location line="+16"/>
+        <source>The collection file &apos;%1&apos; already exists!</source>
+        <translation>모음집 파일 &apos;%1&apos;이(가) 이미 존재합니다!</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Cannot create directory: %1</source>
+        <translation>디렉터리를 만들 수 없음: %1</translation>
+    </message>
+    <message>
+        <location line="+26"/>
+        <source>Cannot copy collection file: %1</source>
+        <translation>모음집 파일을 복사할 수 없음: %1</translation>
+    </message>
+    <message>
+        <location line="+119"/>
+        <source>Unknown filter &apos;%1&apos;!</source>
+        <translation>알 수 없는 필터 &apos;%1&apos;!</translation>
+    </message>
+    <message>
+        <location line="+53"/>
+        <source>Cannot register filter %1!</source>
+        <translation>필터 &apos;%1&apos;을(를) 등록할 수 없습니다!</translation>
+    </message>
+    <message>
+        <location line="+44"/>
+        <source>Cannot open documentation file %1!</source>
+        <translation>문서 파일 %1을(를) 열 수 없습니다!</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Invalid documentation file &apos;%1&apos;!</source>
+        <translation>문서 파일 &apos;%1&apos;이(가) 잘못되었습니다!</translation>
+    </message>
+    <message>
+        <location line="+34"/>
+        <source>The namespace %1 was not registered!</source>
+        <translation>네임스페이스 %1이(가) 등록되지 않았습니다!</translation>
+    </message>
+    <message>
+        <location line="+120"/>
+        <source>Namespace %1 already exists!</source>
+        <translation>네임스페이스 %1이(가) 이미 존재합니다!</translation>
+    </message>
+    <message>
+        <location line="+13"/>
+        <source>Cannot register namespace &apos;%1&apos;!</source>
+        <translation>네임스페이스 &apos;%1&apos;을(를) 등록할 수 없습니다!</translation>
+    </message>
+    <message>
+        <location line="+24"/>
+        <source>Cannot open database &apos;%1&apos; to optimize!</source>
+        <translation>데이터베이스 &apos;%1&apos;을(를) 최적화하기 위하여 열 수 없습니다!</translation>
+    </message>
+</context>
+<context>
+    <name>QHelpDBReader</name>
+    <message>
+        <location filename="../tools/assistant/lib/qhelpdbreader.cpp" line="+98"/>
+        <source>Cannot open database &apos;%1&apos; &apos;%2&apos;: %3</source>
+        <extracomment>The placeholders are: %1 - The name of the database which cannot be opened %2 - The unique id for the connection %3 - The actual error string</extracomment>
+        <translation>데이터베이스 &apos;%1&apos; &apos;%2&apos;을(를) 열 수 없음: %3</translation>
+    </message>
+</context>
+<context>
+    <name>QHelpEngineCore</name>
+    <message>
+        <location filename="../tools/assistant/lib/qhelpenginecore.cpp" line="+122"/>
+        <source>Cannot open documentation file %1: %2!</source>
+        <translation>문서 파일 %1을(를) 열 수 없음: %2!</translation>
+    </message>
+    <message>
+        <location line="+404"/>
+        <source>The specified namespace does not exist!</source>
+        <translation>지정한 네임스페이스가 존재하지 않습니다!</translation>
+    </message>
+</context>
+<context>
+    <name>QHelpGenerator</name>
+    <message>
+        <location filename="../tools/assistant/lib/qhelpgenerator.cpp" line="+158"/>
+        <source>Invalid help data!</source>
+        <translation>도움말 데이터가 잘못되었습니다!</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>No output file name specified!</source>
+        <translation>출력 파일 이름이 지정되지 않았습니다!</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>The file %1 cannot be overwritten!</source>
+        <translation>파일 %1에 겹쳐쓸 수 없습니다!</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Building up file structure...</source>
+        <translation>파일 구조 생성 중...</translation>
+    </message>
+    <message>
+        <location line="+11"/>
+        <source>Cannot open data base file %1!</source>
+        <translation>데이터베이스 파일 %1을(를) 열 수 없습니다!</translation>
+    </message>
+    <message>
+        <location line="+14"/>
+        <source>Cannot register namespace %1!</source>
+        <translation>네임스페이스 %1을(를) 등록할 수 없습니다!</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Insert custom filters...</source>
+        <translation>사용자 정의 필터 추가...</translation>
+    </message>
+    <message>
+        <location line="+12"/>
+        <source>Insert help data for filter section (%1 of %2)...</source>
+        <translation>필터 섹션 (%2 중 %1)에 도움말 데이터 추가...</translation>
+    </message>
+    <message>
+        <location line="+18"/>
+        <source>Documentation successfully generated.</source>
+        <translation>문서가 성공적으로 생성되었습니다.</translation>
+    </message>
+    <message>
+        <location line="+76"/>
+        <source>Some tables already exist!</source>
+        <translation>일부 테이블이 이미 존재합니다!</translation>
+    </message>
+    <message>
+        <location line="+61"/>
+        <source>Cannot create tables!</source>
+        <translation>테이블을 만들 수 없습니다!</translation>
+    </message>
+    <message>
+        <location line="+86"/>
+        <source>Cannot register virtual folder!</source>
+        <translation>가상 폴더를 등록할 수 없습니다!</translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>Insert files...</source>
+        <translation>파일 추가...</translation>
+    </message>
+    <message>
+        <location line="+42"/>
+        <source>The referenced file %1 must be inside or within a subdirectory of (%2). Skipping it.</source>
+        <translation>참조되는 파일 %1은(는) (%2) 그 자체 및 하위 디렉터리에 있어야 합니다. 건너뜁니다.</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>The file %1 does not exist! Skipping it.</source>
+        <translation>파일 %1이(가) 존재하지 않습니다! 건너뜁니다.</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Cannot open file %1! Skipping it.</source>
+        <translation>파일 %1을(를) 열 수 없습니다! 건너뜁니다.</translation>
+    </message>
+    <message>
+        <location line="+133"/>
+        <source>The filter %1 is already registered!</source>
+        <translation>필터 %1이(가) 이미 등록되었습니다!</translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <source>Cannot register filter %1!</source>
+        <translation>필터 %1을(를) 등록할 수 없습니다!</translation>
+    </message>
+    <message>
+        <location line="+24"/>
+        <source>Insert indices...</source>
+        <translation>인덱스 추가...</translation>
+    </message>
+    <message>
+        <location line="+92"/>
+        <source>Insert contents...</source>
+        <translation>내용 추가...</translation>
+    </message>
+    <message>
+        <location line="+8"/>
+        <source>Cannot insert contents!</source>
+        <translation>내용을 추가할 수 없습니다!</translation>
+    </message>
+    <message>
+        <location line="+12"/>
+        <source>Cannot register contents!</source>
+        <translation>내용을 등록할 수 없습니다!</translation>
+    </message>
+    <message>
+        <location line="+56"/>
+        <source>File &apos;%1&apos; does not exist.</source>
+        <translation>파일 &apos;%1&apos;이(가) 존재하지 않습니다.</translation>
+    </message>
+    <message>
+        <location line="+19"/>
+        <source>File &apos;%1&apos; cannot be opened.</source>
+        <translation>파일 &apos;%1&apos;을(를) 열 수 없습니다.</translation>
+    </message>
+    <message>
+        <location line="+19"/>
+        <source>File &apos;%1&apos; contains an invalid link to file &apos;%2&apos;</source>
+        <translation>파일 &apos;%1&apos;이(가) 파일 &apos;%2&apos;(으)로 가는 올바르지 않은 링크를 포함합니다</translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>Invalid links in HTML files.</source>
+        <translation>HTML 파일에 잘못된 링크가 있습니다.</translation>
+    </message>
+</context>
+<context>
+    <name>QHelpProject</name>
+    <message>
+        <location filename="../tools/assistant/lib/qhelpprojectdata.cpp" line="+88"/>
+        <source>Unknown token.</source>
+        <translation>알 수 없는 토큰.</translation>
+    </message>
+    <message>
+        <location line="+13"/>
+        <source>Unknown token. Expected &quot;QtHelpProject&quot;!</source>
+        <translation>알 수 없는 토큰. &quot;QtHelpProject&quot;를 예상하였습니다!</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Error in line %1: %2</source>
+        <translation>%1줄에 오류가 있음: %2</translation>
+    </message>
+    <message>
+        <location line="+14"/>
+        <source>Virtual folder has invalid syntax.</source>
+        <translation>가상 폴더 문법이 잘못되었습니다.</translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <source>Namespace has invalid syntax.</source>
+        <translation>네임스페이스 문법이 잘못되었습니다.</translation>
+    </message>
+    <message>
+        <location line="+19"/>
+        <source>Missing namespace in QtHelpProject.</source>
+        <translation>QtHelpProject에 네임스페이스가 없습니다.</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Missing virtual folder in QtHelpProject</source>
+        <translation>QtHelpProject에 가상 폴더가 없습니다</translation>
+    </message>
+    <message>
+        <location line="+89"/>
+        <source>Missing attribute in keyword at line %1.</source>
+        <translation>%1줄에 있는 키워드에 속성이 없습니다.</translation>
+    </message>
+    <message>
+        <location line="+143"/>
+        <source>The input file %1 could not be opened!</source>
+        <translation>입력 파일 %1을(를) 열 수 없습니다!</translation>
+    </message>
+</context>
+<context>
+    <name>QHelpSearchQueryWidget</name>
+    <message>
+        <location filename="../tools/assistant/lib/qhelpsearchquerywidget.cpp" line="+124"/>
+        <source>Search for:</source>
+        <translation>찾을 단어:</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Previous search</source>
+        <translation>이전 찾기</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Next search</source>
+        <translation>다음 찾기</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Search</source>
+        <translation>찾기</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>Advanced search</source>
+        <translation>고급 검색</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>words &lt;B&gt;similar&lt;/B&gt; to:</source>
+        <translation>다음과 &lt;B&gt;비슷한&lt;/B&gt; 단어:</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>&lt;B&gt;without&lt;/B&gt; the words:</source>
+        <translation>다음 단어 &lt;B&gt;제외&lt;/B&gt;:</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>with &lt;B&gt;exact phrase&lt;/B&gt;:</source>
+        <translation>다음 &lt;B&gt;정확한 구절&lt;/B&gt;:</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>with &lt;B&gt;all&lt;/B&gt; of the words:</source>
+        <translation>다음 단어 &lt;B&gt;모두&lt;/B&gt; 포함:</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>with &lt;B&gt;at least one&lt;/B&gt; of the words:</source>
+        <translation>다음 단어 중 &lt;B&gt;최소한 한 단어&lt;/B&gt; 포함:</translation>
+    </message>
+</context>
+<context>
+    <name>QHelpSearchResultWidget</name>
+    <message numerus="yes">
+        <location filename="../tools/assistant/lib/qhelpsearchresultwidget.cpp" line="+174"/>
+        <source>%1 - %2 of %n Hits</source>
+        <translation>
+            <numerusform>검색 결과 %n개 중 %1 - %2</numerusform>
+        </translation>
+    </message>
+    <message>
+        <location line="+61"/>
+        <source>0 - 0 of 0 Hits</source>
+        <translation>검색 결과 0개 중 0 - 0</translation>
+    </message>
+</context>
+</TS>
