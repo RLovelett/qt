RLovelett/qt

commit 315c09c3873f7c5d8b785443372bf975bae47ee7

Make sure mappings are created for columns when indexes newly become parents.
Also move the modeltest related tests to the modeltest subdirectory

Merge-request: 2262
Reviewed-by: Olivier Goffart <ogoffart@trolltech.com>

src/gui/itemviews/qsortfilterproxymodel.cpp
@@ -734,6 +734,33 @@ void QSortFilterProxyModelPrivate::source_items_inserted(
         }
     }
 
+    if (model->rowCount(source_parent) == delta_item_count) {
+        // Items were inserted where there were none before.
+        // If it was new rows make sure to create mappings for columns so that a
+        // valid mapping can be retreived later and vice-versa.
+
+        QVector<int> &orthogonal_proxy_to_source = (orient == Qt::Horizontal) ? m->source_rows : m->source_columns;
+        QVector<int> &orthogonal_source_to_proxy = (orient == Qt::Horizontal) ? m->proxy_rows : m->proxy_columns;
+
+        if (orthogonal_source_to_proxy.isEmpty()) {
+            const int ortho_end = (orient == Qt::Horizontal) ? model->rowCount(source_parent) : model->columnCount(source_parent);
+
+            for (int ortho_item = 0; ortho_item < ortho_end; ++ortho_item) {
+                if ((orient == Qt::Horizontal) ? q->filterAcceptsRow(ortho_item, source_parent)
+                        : q->filterAcceptsColumn(ortho_item, source_parent)) {
+                    orthogonal_proxy_to_source.append(ortho_item);
+                }
+            }
+            orthogonal_source_to_proxy.resize(orthogonal_proxy_to_source.size());
+
+            if (orient == Qt::Horizontal) {
+                // We're reacting to columnsInserted, but we've just inserted new rows. Sort them.
+                sort_source_rows(orthogonal_proxy_to_source, source_parent);
+            }
+            build_source_to_proxy_mapping(orthogonal_proxy_to_source, orthogonal_source_to_proxy);
+        }
+    }
+
     // Sort and insert the items
     if (orient == Qt::Vertical) // Only sort rows
         sort_source_rows(source_items, source_parent);

tests/auto/headers/tst_headers.cpp
@@ -217,8 +217,8 @@ void tst_Headers::licenseCheck()
     QCOMPARE(content.at(i++), QString("/****************************************************************************"));
     if (licenseType != "3RDPARTY") {
         QCOMPARE(content.at(i++), QString("**"));
-        if (sourceFile.endsWith("/tests/auto/qabstractitemmodel/dynamictreemodel.cpp")
-            || sourceFile.endsWith("/tests/auto/qabstractitemmodel/dynamictreemodel.h")
+        if (sourceFile.endsWith("/tests/auto/modeltest/dynamictreemodel.cpp")
+            || sourceFile.endsWith("/tests/auto/modeltest/dynamictreemodel.h")
             || sourceFile.endsWith("/src/network/kernel/qnetworkproxy_p.h"))
         {
             // These files are not copyrighted by Nokia.

tests/auto/modeltest/modeltest.cpp
@@ -475,8 +475,8 @@ void ModelTest::data()
 void ModelTest::rowsAboutToBeInserted ( const QModelIndex &parent, int start, int end )
 {
 //     Q_UNUSED(end);
-    qDebug() << "rowsAboutToBeInserted" << "start=" << start << "end=" << end << "parent=" << model->data ( parent ).toString()
-    << "current count of parent=" << model->rowCount ( parent ); // << "display of last=" << model->data( model->index(start-1, 0, parent) );
+//    qDebug() << "rowsAboutToBeInserted" << "start=" << start << "end=" << end << "parent=" << model->data ( parent ).toString()
+//    << "current count of parent=" << model->rowCount ( parent ); // << "display of last=" << model->data( model->index(start-1, 0, parent) );
 //     qDebug() << model->index(start-1, 0, parent) << model->data( model->index(start-1, 0, parent) );
     Changing c;
     c.parent = parent;
@@ -495,14 +495,14 @@ void ModelTest::rowsInserted ( const QModelIndex & parent, int start, int end )
 {
     Changing c = insert.pop();
     Q_ASSERT ( c.parent == parent );
-    qDebug() << "rowsInserted"  << "start=" << start << "end=" << end << "oldsize=" << c.oldSize
-    << "parent=" << model->data ( parent ).toString() << "current rowcount of parent=" << model->rowCount ( parent );
+//    qDebug() << "rowsInserted"  << "start=" << start << "end=" << end << "oldsize=" << c.oldSize
+//    << "parent=" << model->data ( parent ).toString() << "current rowcount of parent=" << model->rowCount ( parent );
 
-    for (int ii=start; ii <= end; ii++)
-    {
-      qDebug() << "itemWasInserted:" << ii << model->data ( model->index ( ii, 0, parent ));
-    }
-    qDebug();
+//    for (int ii=start; ii <= end; ii++)
+//    {
+//      qDebug() << "itemWasInserted:" << ii << model->data ( model->index ( ii, 0, parent ));
+//    }
+//    qDebug();
 
     Q_ASSERT ( c.oldSize + ( end - start + 1 ) == model->rowCount ( parent ) );
     Q_ASSERT ( c.last == model->data ( model->index ( start - 1, 0, c.parent ) ) );

tests/auto/modeltest/modeltest.pro
@@ -1,6 +1,6 @@
 load(qttest_p4)
-SOURCES         += tst_modeltest.cpp modeltest.cpp 
-HEADERS         += modeltest.h
+SOURCES         += tst_modeltest.cpp modeltest.cpp dynamictreemodel.cpp
+HEADERS         += modeltest.h dynamictreemodel.h
 
 
 

tests/auto/modeltest/tst_modeltest.cpp
@@ -44,6 +44,7 @@
 #include <QtGui/QtGui>
 
 #include "modeltest.h"
+#include "dynamictreemodel.h"
 
 
 class tst_ModelTest : public QObject
@@ -64,6 +65,7 @@ private slots:
     void stringListModel();
     void treeWidgetModel();
     void standardItemModel();
+    void testInsertThroughProxy();
 };
 
 
@@ -143,7 +145,38 @@ void tst_ModelTest::standardItemModel()
     
     model.insertRows(0,5, model.index(1,1));
     model.insertColumns(0,5, model.index(1,3));
-    
+}
+
+void tst_ModelTest::testInsertThroughProxy()
+{
+    DynamicTreeModel *model = new DynamicTreeModel(this);
+
+    QSortFilterProxyModel *proxy = new QSortFilterProxyModel(this);
+    proxy->setSourceModel(model);
+
+    new ModelTest(proxy, this);
+
+    ModelInsertCommand *insertCommand = new ModelInsertCommand(model, this);
+    insertCommand->setNumCols(4);
+    insertCommand->setStartRow(0);
+    insertCommand->setEndRow(9);
+    // Parent is QModelIndex()
+    insertCommand->doCommand();
+
+    insertCommand = new ModelInsertCommand(model, this);
+    insertCommand->setNumCols(4);
+    insertCommand->setAncestorRowNumbers(QList<int>() << 5);
+    insertCommand->setStartRow(0);
+    insertCommand->setEndRow(9);
+    insertCommand->doCommand();
+
+    ModelMoveCommand *moveCommand = new ModelMoveCommand(model, this);
+    moveCommand->setNumCols(4);
+    moveCommand->setStartRow(0);
+    moveCommand->setEndRow(0);
+    moveCommand->setDestRow(9);
+    moveCommand->setDestAncestors(QList<int>() << 5);
+    moveCommand->doCommand();
 }
 
 QTEST_MAIN(tst_ModelTest)

tests/auto/qabstractitemmodel/qabstractitemmodel.pro
@@ -1,6 +1,9 @@
 load(qttest_p4)
-SOURCES         += tst_qabstractitemmodel.cpp dynamictreemodel.cpp
-HEADERS         += dynamictreemodel.h
+
+INCLUDEPATH += $$PWD/../modeltest
+
+SOURCES         += tst_qabstractitemmodel.cpp ../modeltest/dynamictreemodel.cpp ../modeltest/modeltest.cpp
+HEADERS         += ../modeltest/dynamictreemodel.h ../modeltest/modeltest.h
 
 
 

commit 437987617e2ef5c152160112b1dddbc53f96788b

Fix filtering of columns in QSFPM.
Fixes a crash introduced in 315c09c3873f7c5d8b785443372bf975bae47ee7

src/gui/itemviews/qsortfilterproxymodel.cpp
@@ -782,14 +782,14 @@ void QSortFilterProxyModelPrivate::source_items_inserted(
         if (orthogonal_source_to_proxy.isEmpty()) {
             const int ortho_end = (orient == Qt::Horizontal) ? model->rowCount(source_parent) : model->columnCount(source_parent);
 
+            orthogonal_source_to_proxy.resize(ortho_end);
+
             for (int ortho_item = 0; ortho_item < ortho_end; ++ortho_item) {
                 if ((orient == Qt::Horizontal) ? q->filterAcceptsRow(ortho_item, source_parent)
                         : q->filterAcceptsColumn(ortho_item, source_parent)) {
                     orthogonal_proxy_to_source.append(ortho_item);
                 }
             }
-            orthogonal_source_to_proxy.resize(orthogonal_proxy_to_source.size());
-
             if (orient == Qt::Horizontal) {
                 // We're reacting to columnsInserted, but we've just inserted new rows. Sort them.
                 sort_source_rows(orthogonal_proxy_to_source, source_parent);

tests/auto/qsortfilterproxymodel/qsortfilterproxymodel.pro
@@ -1,6 +1,7 @@
 load(qttest_p4)
 
-SOURCES += tst_qsortfilterproxymodel.cpp
+INCLUDEPATH += $$PWD/../modeltest
 
-
+SOURCES         += tst_qsortfilterproxymodel.cpp ../modeltest/dynamictreemodel.cpp ../modeltest/modeltest.cpp
+HEADERS         += ../modeltest/dynamictreemodel.h ../modeltest/modeltest.h
 

tests/auto/qsortfilterproxymodel/tst_qsortfilterproxymodel.cpp
@@ -43,6 +43,9 @@
 #include <QtTest/QtTest>
 #include "../../shared/util.h"
 
+#include "dynamictreemodel.h"
+#include "modeltest.h"
+
 #include <QtCore>
 #include <QtGui>
 
@@ -144,6 +147,7 @@ private slots:
 
     void testMultipleProxiesWithSelection();
     void mapSelectionFromSource();
+    void filteredColumns();
 
 protected:
     void buildHierarchy(const QStringList &data, QAbstractItemModel *model);
@@ -3172,6 +3176,41 @@ void tst_QSortFilterProxyModel::taskQTBUG_10287_unnecessaryMapCreation()
     Proxy10287 p(&m);
     m.removeChild();
     // No assert failure, it passes.
+}
+
+class FilteredColumnProxyModel : public QSortFilterProxyModel
+{
+  Q_OBJECT
+public:
+  FilteredColumnProxyModel(QObject *parent = 0)
+    : QSortFilterProxyModel(parent)
+  {
+
+  }
+
+protected:
+  bool filterAcceptsColumn(int column, const QModelIndex &source_parent) const
+  {
+    return column % 2 != 0;
+  }
+};
+
+void tst_QSortFilterProxyModel::filteredColumns()
+{
+    DynamicTreeModel *model = new DynamicTreeModel(this);
+
+    FilteredColumnProxyModel *proxy = new FilteredColumnProxyModel(this);
+    proxy->setSourceModel(model);
+
+    new ModelTest(proxy, this);
+
+    ModelInsertCommand *insertCommand = new ModelInsertCommand(model, this);
+    insertCommand->setNumCols(2);
+    insertCommand->setStartRow(0);
+    insertCommand->setEndRow(0);
+    // Parent is QModelIndex()
+    insertCommand->doCommand();
+
 }
 
 QTEST_MAIN(tst_QSortFilterProxyModel)