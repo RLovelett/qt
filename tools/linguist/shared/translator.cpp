4a17c5c67bedd3188e39c3e27dc63d7c6da978fb
Ryan van der Bijl <ryan@enzosystems.com>
2010-12-03 13:26:50 +0100

    - Fix lupdate guess language from filename so that files like "de_DE.project.ts" will have the language detected

--- tools/linguist/shared/translator.cpp
+++ tools/linguist/shared/translator.cpp
@@ -7,11 +7,11 @@
 ** This file is part of the Qt Linguist of the Qt Toolkit.
 **
 ** $QT_BEGIN_LICENSE:LGPL$
-** No Commercial Usage
-** This file contains pre-release code and may not be distributed.
-** You may use this file in accordance with the terms and conditions
-** contained in the Technology Preview License Agreement accompanying
-** this package.
+** Commercial Usage
+** Licensees holding valid Qt Commercial licenses may use this file in
+** accordance with the Qt Commercial License Agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and Nokia.
 **
 ** GNU Lesser General Public License Usage
 ** Alternatively, this file may be used under the terms of the GNU Lesser
@@ -25,16 +25,16 @@
 ** rights.  These rights are described in the Nokia Qt LGPL Exception
 ** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
 **
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 3.0 as published by the Free Software
+** Foundation and appearing in the file LICENSE.GPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU General Public License version 3.0 requirements will be
+** met: http://www.gnu.org/copyleft/gpl.html.
+**
 ** If you have questions regarding the use of this file, please contact
 ** Nokia at qt-info@nokia.com.
-**
-**
-**
-**
-**
-**
-**
-**
 ** $QT_END_LICENSE$
 **
 ****************************************************************************/
@@ -692,8 +692,10 @@ void Translator::normalizeTranslations(ConversionData &cd)
             "not set or recognized."));
 }
 
-QString Translator::guessLanguageCodeFromFileName(const QString &filename)
+QString Translator::guessLanguageCodeFromFileName(const QString &filenameOrig)
 {
+    // remove directory, and file extension
+    const QString filename = QFileInfo(filenameOrig).fileName();
     QString str = filename;
     foreach (const FileFormat &format, registeredFileFormats()) {
         if (str.endsWith(format.extension)) {
@@ -701,18 +703,19 @@ QString Translator::guessLanguageCodeFromFileName(const QString &filename)
             break;
         }
     }
+
+    // first break apart based only on ".": this is to try to match language_COUNTRY in the filename first.
+    //  Then try separating based on "_" or "." to find more possibilities.
     static QRegExp re(QLatin1String("[\\._]"));
-    while (true) {
-        QLocale locale(str);
-        //qDebug() << "LANGUAGE FROM " << str << "LANG: " << locale.language();
+    QStringList toCheck = str.split(QLatin1String("."), QString::SkipEmptyParts) +
+                          str.split(re, QString::SkipEmptyParts);
+    foreach(const QString &part, toCheck) {
+        QLocale locale(part);
+        //qDebug() << "LANGUAGE FROM " << part << "LANG CODE: " << locale.language();
         if (locale.language() != QLocale::C) {
             //qDebug() << "FOUND " << locale.name();
             return locale.name();
         }
-        int pos = str.indexOf(re);
-        if (pos == -1)
-            break;
-        str = str.mid(pos + 1);
     }
     //qDebug() << "LANGUAGE GUESSING UNSUCCESSFUL";
     return QString();
