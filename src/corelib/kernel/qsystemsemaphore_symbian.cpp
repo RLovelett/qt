RLovelett/qt

commit 570e7b38487455d394b5b74a59edc639f3dc416f

Fix QSystemSemaphore handle management issues in Symbian
QSystemSemaphore native RSemaphore handle was unnecessarily created
again on each acquire. The creation logic was also susceptible for
random failures when two or more threads tried simultaneously acquire
the semaphore.

Task-number: QTBUG-16615
Reviewed-by: Janne Koskinen

src/corelib/kernel/qsystemsemaphore_symbian.cpp
@@ -73,6 +73,9 @@ void QSystemSemaphorePrivate::setErrorString(const QString &function, int err)
     case KErrInUse:
         errorString = QCoreApplication::tr("%1: out of resources", "QSystemSemaphore").arg(function);
         error = QSystemSemaphore::OutOfResources;
+    case KErrPermissionDenied:
+        errorString = QCoreApplication::tr("%1: permission denied", "QSystemSemaphore").arg(function);
+        error = QSystemSemaphore::PermissionDenied;
     break;
 default:
     errorString = QCoreApplication::tr("%1: unknown error %2", "QSystemSemaphore").arg(function).arg(err);
@@ -86,15 +89,24 @@ default:
 
 int QSystemSemaphorePrivate::handle(QSystemSemaphore::AccessMode)
 {
+    if (semaphore.Handle()) {
+        return semaphore.Handle();
+    }
+
     // don't allow making handles on empty keys
     if (key.isEmpty())
         return 0;
+
     QString safeName = makeKeyFileName();
     TPtrC name(qt_QString2TPtrC(safeName));
-    int err;
-    err = semaphore.OpenGlobal(name,EOwnerProcess);
-    if (err == KErrNotFound){
-        err = semaphore.CreateGlobal(name,initialValue, EOwnerProcess);
+    int err = KErrAlreadyExists;
+    int tryCount = 10;
+    // Sort out race conditions by retrying several times until existing handle is acquired.
+    // Sometimes opening can fail inexplicably with KErrPermissionDenied many times in a row.
+    while (err != KErrNoMemory && err != KErrNone && tryCount-- >= 0) {
+        err = semaphore.CreateGlobal(name, initialValue, EOwnerProcess);
+        if (err != KErrNoMemory && err != KErrNone)
+            err = semaphore.OpenGlobal(name,EOwnerProcess);
     }
     if (err){
         setErrorString(QLatin1String("QSystemSemaphore::handle"),err);

commit eff99751c9c2bd932b5caa091b28cf3cdc9e1c7e

fix error reporting
that was mistakenly broken by 570e7b38487455d394b5b74a59edc639f3dc416f

src/corelib/kernel/qsystemsemaphore_symbian.cpp
@@ -73,6 +73,7 @@ void QSystemSemaphorePrivate::setErrorString(const QString &function, int err)
     case KErrInUse:
         errorString = QCoreApplication::tr("%1: out of resources", "QSystemSemaphore").arg(function);
         error = QSystemSemaphore::OutOfResources;
+        break;
     case KErrPermissionDenied:
         errorString = QCoreApplication::tr("%1: permission denied", "QSystemSemaphore").arg(function);
         error = QSystemSemaphore::PermissionDenied;