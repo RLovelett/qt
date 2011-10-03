72fdb5b0c1a8c8ab7016b224cc0a5567621d9a7c
jaanttil <janne.anttila@digia.com>
2011-10-03 16:17:42 +0300

    Fixed zlib build for WEC7.
    errno in zutil.c is leftover, and not used anymore -> removed.
    
    Changes to gzguts.h are copied from qfunctions_wince.h file, better
    approach would be to use qfunctions_wince.h directly, like it has been
    done in libtiff. However including qfunctions_wince.h causes some
    other problems, that's why using this workaround for now.
    
    Task-number: QTBUG-22507

--- src/3rdparty/zlib/gzguts.h
+++ src/3rdparty/zlib/gzguts.h
@@ -33,6 +33,40 @@
 #endif
 #if !defined(_WIN32_WCE)
 #include <fcntl.h>
+#else
+#include <netwdm.h> // For io.h type WORK_QUEUE_TYPE
+
+extern  long    qt_wince__lseek( int handle, long offset, int origin );
+extern  int     qt_wince__read( int handle, void *buffer, unsigned int count );
+extern  int     qt_wince__write( int handle, const void *buffer, unsigned int count );
+extern  int     qt_wince__close( int handle );
+#ifdef __cplusplus
+extern  int     qt_wince_open( const char *filename, int oflag, int pmode = 0 );
+#else
+extern  int     qt_wince_open( const char *filename, int oflag, int pmode );
+#endif
+
+#define open qt_wince_open
+#define close qt_wince__close
+#define lseek qt_wince__lseek
+#define read qt_wince__read
+#define write qt_wince__write
+
+#define _O_RDONLY       0x0001
+#define _O_RDWR         0x0002
+#define _O_WRONLY       0x0004
+#define _O_CREAT        0x0008
+#define _O_TRUNC        0x0010
+#define _O_APPEND       0x0020
+#define _O_EXCL         0x0040
+
+#define O_RDONLY        _O_RDONLY
+#define O_RDWR          _O_RDWR
+#define O_WRONLY        _O_WRONLY
+#define O_CREAT         _O_CREAT
+#define O_TRUNC         _O_TRUNC
+#define O_APPEND        _O_APPEND
+#define O_EXCL          _O_EXCL
 #endif
 
 #ifdef NO_DEFLATE       /* for compatibility with old definition */
