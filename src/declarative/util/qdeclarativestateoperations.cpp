a46b9741a879688c4ceaad82eeb79c32f66ba5bc
jaanttil <janne.anttila@digia.com>
2011-10-05 09:56:11 +0300

    Build fix for QtDeclarative in WindowsCE
    qdeclarativestateoperations.cpp in declarative module needs to include
    qgraphicsitem.h. In windows CE, the qgraphicsitem.h further needs to
    include qfunctions_wince.h. qfunctions_wince.h is used to implement
    posix functions missing from WinCE. One of reimplemented functions in
    qfunctions_wince.h is posix rewind. Now qdeclarativestateoperations.cpp
    also implements method called rewind with different prototype. Two
    incompatible prototypes of rewind break the QtDeclarative build
    for WinCE.
    
    As a workaround the POSIX rewind reimplemented by qfunctions_wince.h
    is now undefined in this class, because it is not needed here.
    
    Task-number: QTBUG-22511

--- src/declarative/util/qdeclarativestateoperations.cpp
+++ src/declarative/util/qdeclarativestateoperations.cpp
@@ -56,6 +56,16 @@
 
 #include <QtCore/qdebug.h>
 #include <QtGui/qgraphicsitem.h>
+#ifdef Q_OS_WINCE
+// qgraphicsitem.h includes qfunctions_wince.h.
+// qfunctions_wince.h defines a missing posix rewind for WinCE,
+// but this conflicts with rewind method defined in this class.
+// As a workaround we undefine WinCE posix replacement for rewind here.
+// Better approach would be to remove qfunctions_wince.h include from
+// qgraphicsitem.h, but unfortunately that wasn't possible due 
+// to other dependencies.
+#   undef rewind
+#endif
 #include <QtCore/qmath.h>
 
 #include <private/qobject_p.h>
